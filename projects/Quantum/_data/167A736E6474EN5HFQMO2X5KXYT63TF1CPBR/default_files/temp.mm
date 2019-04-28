a19801b46024f9db32c9fc5f83a1db01e75c4cfc
goldenfibbo <dongkai.pan@gmail.com>
2019-04-28 11:33:35 +0800

    cut nonsense

--- projects/Quantum/_data/167A736E6474EN5HFQMO2X5KXYT63TF1CPBR/default_files/temp.mm
+++
@@ -1,6 +0,0 @@
-<map version="freeplane 1.3.0" project="167A736E6474EN5HFQMO2X5KXYT63TF1CPBR">
-<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
-<node TEXT="Temp" FOLDED="false" ID="ID_1194995804" CREATED="1544698890153" MODIFIED="1544698890155">
-<hook NAME="AutomaticEdgeColor" COUNTER="0"/>
-</node>
-</map>
