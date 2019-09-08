d21e28b3671a5c632d33657defb8df30514bd019
dk.pan <thoughts@thoughts.localdomain>
2019-09-08 13:59:54 +0800

    add tricks for math

---
+++ projects/mathematics/tricks.mm
@@ -0,0 +1,76 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Tricks for Math" FOLDED="false" ID="ID_1535341287" CREATED="1567909846452" MODIFIED="1567909871994" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="2.143">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="\latex $\nabla$" POSITION="right" ID="ID_1713858765" CREATED="1567909873501" MODIFIED="1567909923639">
+<edge COLOR="#ff0000"/>
+<node TEXT="\latex $\nabla\times(\boldsymbol E+\boldsymbol F)=\nabla\times \boldsymbol E +\nabla\times \boldsymbol F$&#xa;\\(&#x6570;&#x4e58;,&#x70b9;&#x4e58;,&#x53c9;&#x4e58;&#x90fd;&#x662f;&#x7ebf;&#x6027;&#x8fd0;&#x7b97;)" ID="ID_1836897514" CREATED="1567909925219" MODIFIED="1567910191949"/>
+<node TEXT="\latex $\nabla\cdot(u\boldsymbol E)=(\nabla u)\cdot\boldsymbol E+u(\nabla\cdot \boldsymbol E)$" ID="ID_405173705" CREATED="1567910194020" MODIFIED="1567910336650"/>
+<node TEXT="\latex $\nabla\times(u\boldsymbol E)=(\nabla u)\times\boldsymbol E+u(\nabla\times\boldsymbol E)$" ID="ID_1449506492" CREATED="1567910338075" MODIFIED="1567910384564"/>
+<node TEXT="\latex $\nabla\cdot(\boldsymbol   E \times \boldsymbol F)= \boldsymbol F\cdot(\nabla\times\boldsymbol E)-\boldsymbol E\cdot(\nabla\times\boldsymbol F)$" ID="ID_1105677800" CREATED="1567910444140" MODIFIED="1567910582852"/>
+<node TEXT="\latex $\nabla\times(\boldsymbol E\times \boldsymbol F)=(\boldsymbol F \cdot\nabla)\boldsymbol E-\boldsymbol F(\nabla\cdot\boldsymbol E)-(\boldsymbol E\cdot \nabla)  $" ID="ID_885291927" CREATED="1567910622102" MODIFIED="1567922289766"/>
+</node>
+</node>
+</map>
