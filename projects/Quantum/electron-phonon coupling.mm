7bfad944fbcb33a28c8051fff8a159e3d9497e3e
goldenfibbo <dongkai.pan@gmail.com>
2020-01-10 20:53:34 +0800

    0110

---
+++ projects/Quantum/electron-phonon coupling.mm
@@ -0,0 +1,127 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="electron-phonon coupling(EPC)(non-magnetic)" FOLDED="false" ID="ID_1714064381" CREATED="1578628836869" MODIFIED="1578642333330" STYLE="oval">
+<font NAME="TSCu_Times" SIZE="16"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="TSCu_Times" SIZE="16" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="8" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="among fundamental interactions of quasiparticles in solids" POSITION="right" ID="ID_846200825" CREATED="1578628851365" MODIFIED="1578630034619">
+<edge COLOR="#ff0000"/>
+<font NAME="TSCu_Times" SIZE="16"/>
+<node TEXT="especially in metals, low energy electronic excitations strongly modified by the coupling to lattice vibrations" ID="ID_905211261" CREATED="1578628898038" MODIFIED="1578630034620">
+<font NAME="TSCu_Times" SIZE="16"/>
+<node TEXT="influence their transport and thermodynamic properties" ID="ID_1087454216" CREATED="1578628943365" MODIFIED="1578630034623">
+<font NAME="TSCu_Times" SIZE="16"/>
+</node>
+<node TEXT="provide an attractive electron-electron interaction" ID="ID_1780687100" CREATED="1578629107049" MODIFIED="1578630034625">
+<font NAME="TSCu_Times" SIZE="16"/>
+<node TEXT="origin of the electron pairing underlying superconductivity" ID="ID_1493026573" CREATED="1578629156080" MODIFIED="1578630034626">
+<font NAME="TSCu_Times" SIZE="16"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="basic hamiltonian" POSITION="left" ID="ID_1442529546" CREATED="1578629256740" MODIFIED="1578630034627">
+<edge COLOR="#0000ff"/>
+<font NAME="TSCu_Times" SIZE="16"/>
+<node TEXT="lowest-order process: the scattering of a single electron by a simultaneous creation or annihilation of a single phonon" ID="ID_1781152879" CREATED="1578643136870" MODIFIED="1578643741076">
+<font SIZE="16"/>
+<node TEXT="electron-phonon vertex: the probability for the scattering process" ID="ID_1610931857" CREATED="1578643199785" MODIFIED="1578643741084">
+<font SIZE="16"/>
+</node>
+</node>
+<node TEXT="\latex partial coupling: dynamics of the ions and electrons can be systematically expanded in terms of the small parameter $\kappa= = (m/M)^{1/4}$" ID="ID_256593228" CREATED="1578643357169" MODIFIED="1578643957185">
+<font NAME="TSCu_Times" SIZE="16"/>
+<node TEXT="\latex lowest order in \kappa:&#xa;adiabatic or Born-Oppenheimer approximation &#xa;$\Psi(\mathbf r,\mathbf R)=\chi(\mathbf R)\psi(\mathbf r, \mathbf R)$" ID="ID_1270677412" CREATED="1578644124142" MODIFIED="1578645325781">
+<node TEXT="\latex electronic wavefunction equation:&#xa;\[\begin{aligned} &amp;[T_e+V_ee+H_{e-i}(\mathbf R)]\psi_n(\mathbf{r};\mathbf R)\\&amp;=E_n(\mathbf R)\psi_n(\mathbf r;\mathbf R)\end{aligned}\] &#xa;($\mathbf R$ enter the equation parametrically)" LOCALIZED_STYLE_REF="default" ID="ID_1673400683" CREATED="1578645165780" MODIFIED="1578646485950" HGAP_QUANTITY="42.49999915063384 pt"/>
+</node>
+</node>
+<node TEXT="\latex Fr\ddot ohlich Hamiltonian&#xa;\[ H=H_e+H_{ph}+H_{e_ph}=  \]" ID="ID_462669193" CREATED="1578647570118" MODIFIED="1578651997043">
+<node TEXT="starting point for discussing electron-phonon coupling effects in periodic solids" ID="ID_1902230238" CREATED="1578647623325" MODIFIED="1578647657238"/>
+</node>
+</node>
+<node TEXT="normal state effects in a metal&#xa;focusing on the renormalization of quasiparticles" POSITION="left" ID="ID_1209365801" CREATED="1578629293431" MODIFIED="1578630038513">
+<edge COLOR="#00ff00"/>
+<font NAME="TSCu_Times" SIZE="16"/>
+<node TEXT="allows to experimentally quantify the strength of the interaction" ID="ID_1872501378" CREATED="1578629944028" MODIFIED="1578630034628">
+<font NAME="TSCu_Times" SIZE="16" BOLD="true"/>
+</node>
+</node>
+<node TEXT="phonon-mediated superconductivity" POSITION="right" ID="ID_861104468" CREATED="1578630051444" MODIFIED="1578643741085">
+<edge COLOR="#ff00ff"/>
+<font SIZE="16"/>
+<node TEXT="effective attractive interaction among electrons mediated by phonon exchange is given" ID="ID_49294727" CREATED="1578630140981" MODIFIED="1578643741086">
+<font SIZE="16"/>
+</node>
+<node TEXT="the role of electron-phonon coupling for superconductivity in the context of the strong-coupling Migdal-Eliashberg theory" ID="ID_574805133" CREATED="1578630172690" MODIFIED="1578643741088">
+<font SIZE="16"/>
+</node>
+</node>
+<node TEXT="density-functional based technique to calculate coupling quantities" POSITION="right" ID="ID_273776324" CREATED="1578642239096" MODIFIED="1578643741089">
+<edge COLOR="#7c0000"/>
+<font SIZE="16"/>
+</node>
+<node TEXT="polaron" POSITION="left" ID="ID_1693500911" CREATED="1578647185707" MODIFIED="1578647204062">
+<edge COLOR="#007c00"/>
+</node>
+</node>
+</map>
