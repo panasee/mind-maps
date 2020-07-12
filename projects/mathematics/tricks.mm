c5059001780df5ddd9f542c4503fa1c4ae8013b4
goldenfibbo <dongkai.pan@gmail.com>
2020-07-12 15:00:24 +0800

    update 0712

--- projects/mathematics/tricks.mm
+++ projects/mathematics/tricks.mm
@@ -1,9 +1,9 @@
-<map version="freeplane 1.7.0">
+<map version="freeplane 1.8.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
 <node TEXT="Tricks for Math" FOLDED="false" ID="ID_1535341287" CREATED="1567909846452" MODIFIED="1567909871994" STYLE="oval">
 <font SIZE="18"/>
 <hook NAME="MapStyle" zoom="1.771">
-    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
@@ -63,11 +63,11 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="8" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="9" RULE="ON_BRANCH_CREATION"/>
 <node TEXT="\latex $\nabla$" POSITION="right" ID="ID_1713858765" CREATED="1567909873501" MODIFIED="1567909923639">
 <edge COLOR="#ff0000"/>
-<node TEXT="&#x590d;&#x5408;&#x8fd0;&#x7b97;" ID="ID_981798918" CREATED="1567935479872" MODIFIED="1567935488405">
-<node TEXT="\latex $\nabla\times(\boldsymbol E+\boldsymbol F)=\nabla\times \boldsymbol E +\nabla\times \boldsymbol F$&#xa;\\(&#x6570;&#x4e58;,&#x70b9;&#x4e58;,&#x53c9;&#x4e58;&#x90fd;&#x662f;&#x7ebf;&#x6027;&#x8fd0;&#x7b97;)" ID="ID_1836897514" CREATED="1567909925219" MODIFIED="1567910191949"/>
+<node TEXT="复合运算" ID="ID_981798918" CREATED="1567935479872" MODIFIED="1567935488405">
+<node TEXT="\latex $\nabla\times(\boldsymbol E+\boldsymbol F)=\nabla\times \boldsymbol E +\nabla\times \boldsymbol F$&#xa;\\(数乘,点乘,叉乘都是线性运算)" ID="ID_1836897514" CREATED="1567909925219" MODIFIED="1567910191949"/>
 <node TEXT="\latex $\nabla\cdot(u\boldsymbol E)=(\nabla u)\cdot\boldsymbol E+u(\nabla\cdot \boldsymbol E)$" ID="ID_405173705" CREATED="1567910194020" MODIFIED="1567910336650"/>
 <node TEXT="\latex $\nabla\times(u\boldsymbol E)=(\nabla u)\times\boldsymbol E+u(\nabla\times\boldsymbol E)$" ID="ID_1449506492" CREATED="1567910338075" MODIFIED="1567910384564"/>
 <node TEXT="\latex $\nabla\cdot(\boldsymbol   E \times \boldsymbol F)= \boldsymbol F\cdot(\nabla\times\boldsymbol E)-\boldsymbol E\cdot(\nabla\times\boldsymbol F)$" ID="ID_1105677800" CREATED="1567910444140" MODIFIED="1567910582852"/>
@@ -77,13 +77,13 @@
 <node TEXT="\latex $\nabla\cdot(\nabla\times\boldsymbol E)=0$" ID="ID_880020635" CREATED="1567930982706" MODIFIED="1567931013697"/>
 <node TEXT="\latex $\nabla\times(\nabla\times\boldsymbol E)=\nabla(\nabla\cdot \boldsymbol E)-\nabla^2\boldsymbol E$" ID="ID_1570809047" CREATED="1567931018581" MODIFIED="1567931082380"/>
 </node>
-<node TEXT="&#x5750;&#x6807;&#x7cfb;" ID="ID_1038678354" CREATED="1567935511537" MODIFIED="1573201882423">
-<node TEXT="\latex &#x7b1b;&#x5361;&#x5c14;&#x5750;&#x6807;&#x7cfb;$(x,y,z) $" ID="ID_1008424276" CREATED="1567935533686" MODIFIED="1567935615671"/>
-<node TEXT="\latex &#x67f1;&#x5750;&#x6807;&#x7cfb;$(r,\theta,z)$" ID="ID_274156450" CREATED="1567935547680" MODIFIED="1567935636802">
+<node TEXT="坐标系" ID="ID_1038678354" CREATED="1567935511537" MODIFIED="1573201882423">
+<node TEXT="\latex 笛卡尔坐标系$(x,y,z) $" ID="ID_1008424276" CREATED="1567935533686" MODIFIED="1567935615671"/>
+<node TEXT="\latex 柱坐标系$(r,\theta,z)$" ID="ID_274156450" CREATED="1567935547680" MODIFIED="1567935636802">
 <node TEXT="\latex $\nabla=(\frac{\partial}{\partial r},\frac1r\frac\partial{\partial\theta},\frac\partial{\partial z})" ID="ID_1274581176" CREATED="1567935577608" MODIFIED="1567935717005"/>
 <node TEXT="\latex $\nabla^2=\frac{\partial^2}{\partial r^2}+\frac1r\frac\partial{\partial r}+\frac1{r^2}\frac{\partial^2}{\partial \theta^2}+\frac{\partial^2}{\partial z^2}\\&#xa;=\frac1r\frac\partial{\partial r}(r\frac\partial{\partial r})+\frac1{r^2}\frac{\partial^2}{\partial \theta^2}+\frac{\partial^2}{\partial z^2}$" ID="ID_638522561" CREATED="1567935722133" MODIFIED="1567936065589"/>
 </node>
-<node TEXT="\latex &#x7403;&#x5750;&#x6807;&#x7cfb;$(r,\phi,\theta)$" ID="ID_250103378" CREATED="1567935554241" MODIFIED="1567935657718">
+<node TEXT="\latex 球坐标系$(r,\phi,\theta)$" ID="ID_250103378" CREATED="1567935554241" MODIFIED="1567935657718">
 <node TEXT="\latex $\nabla=(\frac\partial{\partial r},\frac1r\frac\partial{\partial \phi},\frac1r\frac\partial{\partial\theta})$" ID="ID_535876361" CREATED="1567936134741" MODIFIED="1567936230307"/>
 <node TEXT="\latex $\nabla^2=\frac1{r^2}\frac\partial{\partial r}(r^2\frac\partial{\partial r})+\frac1{r^2\sin\theta}\frac\partial{\partial\theta}(\sin\theta\frac\partial{\partial\theta})+&#xa;\frac1{r^2\sin^2\theta}\frac{\partial^2}{\partial\phi^2}$" ID="ID_1478976100" CREATED="1567936233829" MODIFIED="1567936438442"/>
 </node>
@@ -94,32 +94,35 @@
 <node TEXT="\latex $\det(A)=\epsilon_{i_1i_2\cdots i_n}A_{1i_1}\cdots A_{ni_n} $ \\&#xa;$\det(A)=\sum_{\sigma\in S_n}\text{sgn}(\sigma)\prod_{i=1}^{n}a_{i,\sigma(i)}$" ID="ID_1176172171" CREATED="1567933574463" MODIFIED="1567934856182"/>
 <node TEXT="\latex $\boldsymbol a\times \boldsymbol b=\epsilon_{ijk}\boldsymbol x_i a_j b_k$" ID="ID_1352174980" CREATED="1567934223363" MODIFIED="1567934324515"/>
 </node>
-<node TEXT="Fourier&#x53d8;&#x6362;" POSITION="right" ID="ID_1018783350" CREATED="1568449654373" MODIFIED="1568449662344">
+<node TEXT="Fourier变换" POSITION="right" ID="ID_1018783350" CREATED="1568449654373" MODIFIED="1568449662344">
 <edge COLOR="#ff00ff"/>
-<node TEXT="Fourier&#x7ea7;&#x6570;&#x6536;&#x655b;&#x6027;" ID="ID_1293661979" CREATED="1568449681445" MODIFIED="1568449691469">
-<node TEXT="Dirichlet&#x5b9a;&#x7406;(&#x5145;&#x5206;&#x975e;&#x5fc5;&#x8981;)" ID="ID_1662748674" CREATED="1568449691836" MODIFIED="1568449984317">
-<node TEXT="&#x9664;&#x4e86;&#x6709;&#x9650;&#x4e2a;&#x70b9;&#x5916;&#x6709;&#x5b9a;&#x4e49;&#x4e14;&#x4e3a;&#x5355;&#x503c;" ID="ID_138884159" CREATED="1568449787947" MODIFIED="1568449817112"/>
-<node TEXT="&#x5468;&#x671f;&#x51fd;&#x6570;" ID="ID_1200810785" CREATED="1568449817610" MODIFIED="1568449829112"/>
-<node TEXT="\latex $f(x),f^\prime(x)$&#x5206;&#x6bb5;&#x8fde;&#x7eed;\\&#xa;(&#x95f4;&#x65ad;&#x70b9;&#x7ea7;&#x6570;&#x6536;&#x655b;&#x4e8e;&#x5de6;&#x53f3;&#x6781;&#x9650;&#x7684;&#x5e73;&#x5747;)" ID="ID_362302554" CREATED="1568449829582" MODIFIED="1568452776131"/>
+<node TEXT="Fourier级数收敛性" ID="ID_1293661979" CREATED="1568449681445" MODIFIED="1568449691469">
+<node TEXT="Dirichlet定理(充分非必要)" ID="ID_1662748674" CREATED="1568449691836" MODIFIED="1568449984317">
+<node TEXT="除了有限个点外有定义且为单值" ID="ID_138884159" CREATED="1568449787947" MODIFIED="1568449817112"/>
+<node TEXT="周期函数" ID="ID_1200810785" CREATED="1568449817610" MODIFIED="1568449829112"/>
+<node TEXT="\latex $f(x),f^\prime(x)$分段连续\\&#xa;(间断点级数收敛于左右极限的平均)" ID="ID_362302554" CREATED="1568449829582" MODIFIED="1568452776131"/>
 </node>
-<node TEXT="&#x5145;&#x8981;&#x6761;&#x4ef6;&#x5c1a;&#x672a;&#x77e5;(2012)" ID="ID_1124195073" CREATED="1568449712832" MODIFIED="1568449775781"/>
+<node TEXT="充要条件尚未知(2012)" ID="ID_1124195073" CREATED="1568449712832" MODIFIED="1568449775781"/>
 </node>
 </node>
 <node TEXT="coordinate and vector calculation" POSITION="left" ID="ID_1505847399" CREATED="1573201054764" MODIFIED="1573201375985">
 <edge COLOR="#00ffff"/>
-<node TEXT="\latex &#x5411;&#x91cf;&#x7684;&#x6570;&#x5b57;&#x8868;&#x793a;&#x76f8;&#x5173;&#x6cd5;&#x5219;&#x90fd;&#x4e0e;&#x5750;&#x6807;&#x7cfb;&#x6709;&#x5173;\\&#xa;$$\vec a \cdot \vec b = a_i b_i$$ &#x53ea;&#x9650;&#x4e8e;Cartesian&#x5750;&#x6807;&#x7cfb;" ID="ID_1890731029" CREATED="1573201376370" MODIFIED="1573201738787"/>
+<node TEXT="\latex 向量的数字表示相关法则都与坐标系有关\\&#xa;$$\vec a \cdot \vec b = a_i b_i$$ 只限于Cartesian坐标系" ID="ID_1890731029" CREATED="1573201376370" MODIFIED="1573201738787"/>
 </node>
-<node TEXT="&#x5750;&#x6807;&#x53d8;&#x6362;" POSITION="left" ID="ID_1426772472" CREATED="1573201887536" MODIFIED="1573201897081">
+<node TEXT="坐标变换" POSITION="left" ID="ID_1426772472" CREATED="1573201887536" MODIFIED="1573201897081">
 <edge COLOR="#7c0000"/>
 <node TEXT="\latex $\frac{\partial}{\partial y}=\frac{\partial z_i}{\partial y}\frac{\partial}{\partial z_i}\neq \frac{\frac{\partial}{\partial z_i}}{\frac{\partial y}{\partial z_i}} $" ID="ID_613147524" CREATED="1573201897851" MODIFIED="1573202031903"/>
 </node>
-<node TEXT="\latex &#x5b88;&#x6052;&#x8fde;&#x7eed;&#x6027;&#x65b9;&#x7a0b;\\&#xa;$\text{\alpha\ conserves}\Rightarrow -\oint \vec j_\alpha\cdot\vec{dS}-\int\dot\rho_\alpha dV+\int O_\alpha dV=0 $ \\&#xa;$\Leftrightarrow\oint k\nabla\phi_\alpha\cdot \vec{dS}-\int\dot\rho_\alpha dV+\int O_\alpha dV=0$\\&#xa;$(j_\alpha\rightarrow flow;\ \rho_\alpha\rightarrow density;\ O_\alpha\rightarrow input)$" POSITION="right" ID="ID_1949046605" CREATED="1573202176620" MODIFIED="1573202693546">
+<node TEXT="\latex 守恒连续性方程\\&#xa;$\text{\alpha\ conserves}\Rightarrow -\oint \vec j_\alpha\cdot\vec{dS}-\int\dot\rho_\alpha dV+\int O_\alpha dV=0 $ \\&#xa;$\Leftrightarrow\oint k\nabla\phi_\alpha\cdot \vec{dS}-\int\dot\rho_\alpha dV+\int O_\alpha dV=0$\\&#xa;$(j_\alpha\rightarrow flow;\ \rho_\alpha\rightarrow density;\ O_\alpha\rightarrow input)$" POSITION="right" ID="ID_1949046605" CREATED="1573202176620" MODIFIED="1573202693546">
 <edge COLOR="#00007c"/>
 </node>
-<node TEXT="&#x53d8;&#x91cf;&#x4ee3;&#x6362;" POSITION="left" ID="ID_1968524742" CREATED="1582191911576" MODIFIED="1582191931438">
+<node TEXT="变量代换" POSITION="left" ID="ID_1968524742" CREATED="1582191911576" MODIFIED="1582191931438">
 <edge COLOR="#007c00"/>
-<node TEXT="\latex &#x96c5;&#x5404;&#x6bd4;&#x884c;&#x5217;&#x5f0f;&#x975e;&#x96f6;&#x8868;&#x793a;\\&#xa;&#x4e24;&#x4e2a;&#x65b0;&#x53d8;&#x91cf;&#x7ebf;&#x6027;&#x65e0;&#x5173; \\&#xa;$\frac{\partial(\xi, \eta)}{\partial(x, y)}\ne0$" ID="ID_1688054303" CREATED="1582191945937" MODIFIED="1582192072322"/>
-<node TEXT="\latex &#x5bf9;&#x4e8e;&#x4e8c;&#x5143;&#x65b9;&#x7a0b;,&#x5982;&#x679c;&#x964d;&#x7ef4;(&#x4e24;&#x4e2a;&#x53d8;&#x91cf;&#x4e4b;&#x95f4;&#x5efa;&#x7acb;&#x5173;&#x7cfb;)&#x9700;&#x6ce8;&#x610f;&#x504f;&#x5fae;&#x5206;\\&#x4e0e;&#x5168;&#x5fae;&#x5206;&#x7684;&#x533a;&#x5206;&#x4ee5;&#x53ca;&#x540c;&#x7ec4;&#x53d8;&#x91cf;&#x4e4b;&#x95f4;&#x504f;&#x5fae;&#x5206;&#x4e0d;&#x80fd;&#x76f8;&#x4e92;&#x4ee3;&#x6362;.\\&#xa;\[ \frac{d}{dx_i}=\frac{\partial}{\partial x_i}+\Sigma^\prime\frac{\partial}{\partial x_j}\frac{dx_j}{dx_i},\;\frac\partial{\partial x}\ne\frac\partial{\partial y}\frac{dy}{dx} \]" ID="ID_1451971717" CREATED="1582346345062" MODIFIED="1582352732196"/>
+<node TEXT="\latex 雅各比行列式非零表示\\&#xa;两个新变量线性无关 \\&#xa;$\frac{\partial(\xi, \eta)}{\partial(x, y)}\ne0$" ID="ID_1688054303" CREATED="1582191945937" MODIFIED="1582192072322"/>
+<node TEXT="\latex 对于二元方程,如果降维(两个变量之间建立关系)需注意偏微分\\与全微分的区分以及同组变量之间偏微分不能相互代换.\\&#xa;\[ \frac{d}{dx_i}=\frac{\partial}{\partial x_i}+\Sigma^\prime\frac{\partial}{\partial x_j}\frac{dx_j}{dx_i},\;\frac\partial{\partial x}\ne\frac\partial{\partial y}\frac{dy}{dx} \]" ID="ID_1451971717" CREATED="1582346345062" MODIFIED="1582352732196"/>
+</node>
+<node TEXT="\latex Leibniz&apos;s rule(变限积分)\\&#xa;$\frac{d}{dx}\int_{a(x)}^{b(x)}f(x,t)dt=f(x,b(x))\cdot \frac{d}{dx}b(x)-f(x,a(x))\cdot \frac{d}{dx}a(x)\\+\int_{a(x)}^{b(x)}\frac{\partial }{\partial x}f(x,t)dt$" POSITION="left" ID="ID_70083636" CREATED="1594478785091" MODIFIED="1594480553177">
+<edge COLOR="#7c007c"/>
 </node>
 </node>
 </map>
