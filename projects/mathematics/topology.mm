5bd96aeacf4e91a19ef51279bc41f248ab2a61f3
goldenfibbo <dongkai.pan@gmail.com>
2020-11-02 20:05:21 +0800

    1102

---
+++ projects/mathematics/topology.mm
@@ -0,0 +1,71 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="\latex TOPOLOGY&#xa;\\\small{Basic Object: Topological spaces}&#xa;\\ \small{Basic Map: Continuous functions}" FOLDED="false" ID="ID_292535781" CREATED="1604314679091" MODIFIED="1604317147495"><hook NAME="MapStyle" zoom="2.143">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="\latex definition: let $X$ be a set of points. A collection of subsets $U=\{ U_\alpha \}$&#xa; forms a topology on $X$(($X,U$)is a topological space) if:\\&#xa;\checkmark \small{Any \textbf{arbitrary}\ union of $U_\alpha$ is another set in collection $U$} \\&#xa;\checkmark \small{The intersection of any finite number of sets $U_\alpha$} is another set in $U$\\&#xa;\checkmark \small{Both the empty set $\phi$ and the whole space $X$ must be in $U$} \\&#xa;\small{(while a \sigma-algebra only needs closure under \textbf{countable}\ union)}" POSITION="right" ID="ID_1064699452" CREATED="1604316243531" MODIFIED="1604318035895">
+<node TEXT="\latex $U_\alpha \in U$ is called \textbf{open sets},&#xa;and\\ a set $C$ is closed if $(X-C)$ is open" ID="ID_1188651495" CREATED="1604318065689" MODIFIED="1604318554040"/>
+<node TEXT="\latex $A\in X$, the induced topology on $A$ is described by&#xa;\\letting the open sets on $A$ be all sets of the form\\&#xa; $U\cap A$($U$ is an open set in $X$)" ID="ID_1332046959" CREATED="1604318244744" MODIFIED="1604318417104"/>
+<node TEXT="\latex a collection $\Sigma=\{ U_\alpha \}$ of open sets is said to be an \textbf{open cover} of a subset $A$ if $A$ is contained in the union of the $U_\alpha$" ID="ID_1767515993" CREATED="1604318642170" MODIFIED="1604318706199"/>
+<node TEXT="\latex $A\in X$, $A$ is compact if given any open cover of $A$,\\&#xa; there is a finite subcover" ID="ID_900735745" CREATED="1604318447465" MODIFIED="1604318500693"/>
+</node>
+</node>
+</map>
