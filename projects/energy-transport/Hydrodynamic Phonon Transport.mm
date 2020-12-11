95be33367de8418d43428e475737429a2b8abbc4
DongkaiPan <dongkai.pan@gmail.com>
2020-12-11 15:38:48 +0800

    1211

---
+++ projects/energy-transport/Hydrodynamic Phonon Transport.mm
@@ -0,0 +1,85 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Hydrodynamic Phonon Transport" FOLDED="false" ID="ID_503587242" CREATED="1607652854095" MODIFIED="1607652879608" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.61">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="between ballistic and diffusive transport" POSITION="right" ID="ID_1010483564" CREATED="1607652879939" MODIFIED="1607652916420">
+<edge COLOR="#ff0000"/>
+<node TEXT="the R-scattering(U-scattering and isotope/impurity -scattering) which breaks crystal momentum need to be relatively small compared to N-scattering" ID="ID_1209272481" CREATED="1607653198883" MODIFIED="1607654089261">
+<node TEXT="so the temperature range is narrow" ID="ID_1064264164" CREATED="1607653689171" MODIFIED="1607653700699"/>
+</node>
+</node>
+<node TEXT="drift motion" POSITION="left" ID="ID_912524512" CREATED="1607652960483" MODIFIED="1607652964596">
+<edge COLOR="#0000ff"/>
+<node TEXT="displaced distribution function" ID="ID_1342159491" CREATED="1607653710059" MODIFIED="1607653718940">
+<node TEXT="\latex Bose-Einstein \\&#xa;$N_{BE}^d=\frac1{\exp{(\frac{\hbar(\omega-\vec q\cdot \vec u)}{k_BT})}-1}$" ID="ID_1052193876" CREATED="1607653727187" MODIFIED="1607653809891"/>
+</node>
+</node>
+<node TEXT="phonon Poiseuille flow&#xa;and second sound" POSITION="right" ID="ID_576031868" CREATED="1607652990259" MODIFIED="1607653196387">
+<edge COLOR="#00ff00"/>
+<hook URI="attachments/hydrodynamic-diffusive.png" SIZE="0.3772615" NAME="ExternalObject"/>
+<node TEXT="there are heat pulses propagating like a pressure pulse in a fluid(ordinary sound) without significant damping" ID="ID_1810190703" CREATED="1607653106603" MODIFIED="1607653153804"/>
+</node>
+</node>
+</map>
