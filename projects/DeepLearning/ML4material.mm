fd1962e336dcba1361f37fe6d0cbed750d6201fd
panasee <dongkai.pan@gmail.com>
2021-11-08 15:24:51 +0800

    1108

---
+++ projects/DeepLearning/ML4material.mm
@@ -0,0 +1,126 @@
+<map version="freeplane 1.9.8">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="ML for&#xa;material predicting" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1636279423860">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#002b36" zoom="2.852">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/dark_solarized_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1664261437" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#002b36" BACKGROUND_COLOR="#fdf6e3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt" MAX_WIDTH="10 cm" MIN_WIDTH="0 cm">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#fdf6e3" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1664261437" STARTINCLINATION="132.75 pt;-32.25 pt;" ENDINCLINATION="132.75 pt;24 pt;" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="Times New Roman" SIZE="14" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#fdf6e3" BACKGROUND_COLOR="#073642">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#fdf6e3">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#2aa198" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#fdf6e3" BACKGROUND_COLOR="#d33682" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#d33682"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1940777954" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_1940777954"/>
+<font NAME="Ubuntu" SIZE="12" BOLD="true"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#002b36" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#fdf6e3" BACKGROUND_COLOR="#073642" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#fdf6e3" BACKGROUND_COLOR="#586e75" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#fdf6e3" BACKGROUND_COLOR="#657b83" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#fdf6e3" BACKGROUND_COLOR="#839496" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#fdf6e3" BACKGROUND_COLOR="#93a1a1" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#eee8d5" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#eedfcc" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#fdf6e3" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="descriptor&#xa;(mathematical representations of characteristics)" POSITION="right" ID="ID_896313958" CREATED="1636275913195" MODIFIED="1636275956081">
+<node TEXT="one-to-one mapping is best, but also some ML algo can turn non-complete representation into complete one" ID="ID_1086648399" CREATED="1636275985873" MODIFIED="1636279691804">
+<node TEXT="like CNN can intrinsically perform&#xa;further feature extraction/selection" ID="ID_340250778" CREATED="1636280299980" MODIFIED="1636280421446"/>
+</node>
+<node TEXT="e.g." ID="ID_1274622732" CREATED="1636276007545" MODIFIED="1636276009636">
+<node TEXT="atomic configurations" ID="ID_1363035670" CREATED="1636275959272" MODIFIED="1636275975018">
+<node TEXT="\latex SOAP(Smooth Overlap of Atomic Positions)" ID="ID_67205296" CREATED="1636276185233" MODIFIED="1636276268118">
+<node TEXT="\latex 1. construct local atomic density func:\\&#xa;$\rho_i(\boldsymbol r) = \sum_j\delta(\boldsymbol r-\boldsymbol r_j) \cdot f_{cut}(r_{ij})\sim\sum_j\exp (-\frac{|\boldsymbol r-\boldsymbol r_j|^2}{\sigma^2_a})f_{cut}(r_{ij})$" ID="ID_1343267884" CREATED="1636276298545" MODIFIED="1636278154361">
+<node TEXT="permutation&amp;translation invariance is satisfied&#xa;to avoid overcompleteness" ID="ID_1746356818" CREATED="1636277957342" MODIFIED="1636278040182"/>
+<node TEXT="smoothe delta func to a Gaussian func&#xa;to get better numerical behavior" ID="ID_1185283220" CREATED="1636278064866" MODIFIED="1636278096680"/>
+</node>
+<node TEXT="\latex 2. project $\rho_i(\boldsymbol r)$ to radial basis funcs $g_n(r)$ \&amp; spherical harmonics $Y_{lm}(\phi,\theta)$ to get $c_{nlm}^i$:\\&#xa;$\rho_i(\boldsymbol r) = \sum_{nlm} c_{nlm}^i\cdot g_n(r)Y_{lm}(\phi,\theta)$\\&#xa;descriptor vector $\boldsymbol q$ is the power spectrum:\\&#xa;$(\boldsymbol q_i)_{nn^\prime l}=\sum_m(c_{nlm}^i)^*c_{n^\prime lm}^i$" ID="ID_1952552170" CREATED="1636276298545" MODIFIED="1636278465961"/>
+<node TEXT="drawback: computational cost increases dramatically with the types of atoms" ID="ID_820734091" CREATED="1636278489224" MODIFIED="1636278538206"/>
+</node>
+<node TEXT="moment tensor&#xa;(similar accuracy for MLP but much higher&#xa;computational efficiency)" ID="ID_126952470" CREATED="1636278566967" MODIFIED="1636279405837">
+<node TEXT="\latex $M_{\mu\nu}=\sum_j f_\mu(|\boldsymbol r_{ij}|)\cdot \boldsymbol \underbrace{r_{ij}\otimes\cdots\otimes\boldsymbol r_{ij}}\limits_{\nu\ times}$" ID="ID_977367943" CREATED="1636279121835" MODIFIED="1636279278661">
+<node TEXT="\latex $f_{\mu}$ is a radially decaying func" ID="ID_452157170" CREATED="1636279334581" MODIFIED="1636279353944"/>
+</node>
+</node>
+<node TEXT="" ID="ID_506856756" CREATED="1636279590645" MODIFIED="1636279590646">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="atom-centered radial and angular&#xa;symmetry funcs" ID="ID_181489294" CREATED="1636279462008" MODIFIED="1636279502464"/>
+<node TEXT="Weyl matrices" ID="ID_1491202170" CREATED="1636279504271" MODIFIED="1636279508639"/>
+<node TEXT="crystal graphs" ID="ID_1655556809" CREATED="1636279511766" MODIFIED="1636279517114"/>
+<node TEXT="Coulomb matrices" ID="ID_1541830829" CREATED="1636279520579" MODIFIED="1636279530703"/>
+<node TEXT="atomic orbital matrices" ID="ID_1618773701" CREATED="1636279535599" MODIFIED="1636279542304"/>
+<node TEXT="" ID="ID_1929293332" CREATED="1636279590644" MODIFIED="1636279590645">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="most of these suffer from in/over-completeness" ID="ID_1773490612" CREATED="1636279590647" MODIFIED="1636280651776">
+<arrowlink DESTINATION="ID_1086648399" STARTINCLINATION="-74.25 pt;-72 pt;" ENDINCLINATION="60.75 pt;53.25 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="include all properties into a descriptor vector" ID="ID_1386034309" CREATED="1636280997071" MODIFIED="1636281024061">
+<node TEXT="\latex $ X_{ij}=\boldsymbol x^i_j,\; (X_{N_a\times N_x})$\\&#xa;$N_a\rightarrow \text{ no. of atoms},N_x\rightarrow \text{ no. of features}$\\&#xa;each $\boldsymbol x^i$ denotes atom $i$ while $\boldsymbol x^i_j$ denotes its feature $j$" ID="ID_1837646958" CREATED="1636281276922" MODIFIED="1636296939926">
+<node TEXT="\latex each row-vector $\boldsymbol x^i$ can be viewed as a data point in a $N_x$-dimensional feature space\\&#xa;\therefore matrix $X$ can be regarded as a \textbf{probability dist. } in the feature space" ID="ID_1455806304" CREATED="1636296433804" MODIFIED="1636297164037">
+<node TEXT="construct the descriptor with statistical observables such as the mean and std. deviation of the row vectors" ID="ID_119307335" CREATED="1636297190294" MODIFIED="1636297273278"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
