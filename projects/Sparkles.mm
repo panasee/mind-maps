196fdb85a2b30b51e0a19612053baff7cf192345
panasee <dongkai.pan@gmail.com>
2022-05-05 16:16:21 +0800

    0505

---
+++ projects/Sparkles.mm
@@ -0,0 +1,221 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Sparkles" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1651732063788">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8" zoom="1.331">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_nord_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="\latex Non-linear Optics\\&#xa;$\boldsymbol {p(E)}=\alpha^{(1)}\cdot \boldsymbol E + \alpha^{(2)}\boldsymbol{:EE} +\dots$\\&#xa;(电场很大时出现非线性, 比如激光)\\&#xa;(重点是光与物质间的相互作用)" POSITION="right" ID="ID_1588314871" CREATED="1651732132630" MODIFIED="1651734004005">
+<node TEXT="coherent nonlinear optics&#xa;(相干非线性光学)" ID="ID_103838554" CREATED="1651732141004" MODIFIED="1651733183572">
+<node ID="ID_67349875" CREATED="1651732157030" MODIFIED="1651735587465"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>相干调控(coherent control) </b>
+    </p>
+    <p>
+      <b>(利用控制相位来控制结果)</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="strong nonlinear optical effects&#xa;(强非线性光学效应)" ID="ID_863971684" CREATED="1651733559869" MODIFIED="1651733586269">
+<node TEXT="不能用微扰, 对应强耦合, 是物理中最难的问题" ID="ID_1791903058" CREATED="1651733590645" MODIFIED="1651734042156"/>
+</node>
+<node TEXT="photonic crystals(光子晶体)" ID="ID_652549197" CREATED="1651734213200" MODIFIED="1651734253433">
+<node TEXT="不同介电常数的材料周期排列, 存在光子带隙(禁带)" ID="ID_407802535" CREATED="1651734254845" MODIFIED="1651734301628"/>
+</node>
+<node ID="ID_1042682701" CREATED="1651734617971" MODIFIED="1651734657946"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      photonic lattices(光子晶格)
+    </p>
+    <p>
+      (周期性<b>波导</b>阵列)
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="光波在波导中的传播行为可以分解为离散波导模式的叠加" ID="ID_232827501" CREATED="1651734633782" MODIFIED="1651734703650"/>
+<node TEXT="相邻波导间存在隐失波的耦合, 导致新现象" ID="ID_800396584" CREATED="1651734703830" MODIFIED="1651734910432"/>
+<node TEXT="很容易引入非线性, 是研究各种离散现象的平台" ID="ID_398627050" CREATED="1651734910691" MODIFIED="1651734939239"/>
+</node>
+<node ID="ID_1009811571" CREATED="1651735400364" MODIFIED="1651738066198"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>被动非线性光学效应(光与介质间无能量交换):三波混频, 四波混频</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="两个光子与非线性光学材料作用后产生一个新光子(类似有媒介的三粒子散射)" ID="ID_1341913117" CREATED="1651735426017" MODIFIED="1651737753037">
+<node TEXT="\latex \textbf{利用相干性可出现二阶倍频级联(cascading)效应:\\&#xa;相位失配导致不同光子相互干涉产生能量回流&#xa;\\(类比准动量不守恒, $\Delta k = k_{2\omega}-2k_\omega-G_m\neq0$)}" ID="ID_616172174" CREATED="1651736392332" MODIFIED="1651736767640"/>
+</node>
+<node TEXT="金属的对称性禁止了该现象, 然而现实中由于表面处对称性破缺, 该现象被观测所证实" ID="ID_1835037890" CREATED="1651735601489" MODIFIED="1651735672012">
+<node TEXT="表面或者缺陷等对称破缺的特殊位置可以打破材料的对称性限制,带来新现象" ID="ID_1438180745" CREATED="1651735672384" MODIFIED="1651735718093">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+<node TEXT="光孤子(soliton)" ID="ID_633744535" CREATED="1651737008139" MODIFIED="1651737019136">
+<node ID="ID_858609189" CREATED="1651737025075" MODIFIED="1651737086644"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>光学克尔效应 </b>
+    </p>
+    <p>
+      <b>(强激光传播过程中对材料造成影响进而影响自身) </b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="主动非线性光学效应(光与介质间有能量交换):非线性折射&#xa;(折射率非常数, 可导致自聚焦/散焦)" ID="ID_598159232" CREATED="1651737322520" MODIFIED="1651737565964"/>
+<node TEXT="全光开关" ID="ID_1803187015" CREATED="1651738052526" MODIFIED="1651738058973">
+<node ID="ID_620958466" CREATED="1651738114664" MODIFIED="1651738193383"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      光子之间并没有较强的相互作用, 因此只能采用间接方式&quot;以光控光&quot;, 即<b>用一束泵浦光去改变传输信号光的介质, 从而实现对信号光的开关.</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_1171356710" CREATED="1651738194134" MODIFIED="1651738236323"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>声子间也不存在物理意义上的&quot;相互作用&quot;, 声子散射应当也是由材料作为中介实现的,因此可以借鉴表达方式</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="???" POSITION="right" ID="ID_267305220" CREATED="1651732802584" MODIFIED="1651732806712">
+<node TEXT="格波是否存在传播过程,比如如果热流通道狭小,是否会出现格波衍射? 如果材料不均, 是否会有格波折射?" ID="ID_1355686938" CREATED="1651732807514" MODIFIED="1651734995259"/>
+<node TEXT="光子可以被束缚在小空间内导致电场大幅增强, 声子局域化是否同理?" ID="ID_1488298946" CREATED="1651733333291" MODIFIED="1651733397444">
+<node TEXT="单光子非线性光学&#xa;(少数光子与单原子在微共振腔内相互作用&#xa;, 2012物理Nobel)" ID="ID_1678378905" CREATED="1651733404687" MODIFIED="1651733505117"/>
+</node>
+<node TEXT="物理中对强耦合的后缀是&quot;情形&quot;" ID="ID_1277630610" CREATED="1651733942946" MODIFIED="1651733962037"/>
+</node>
+<node TEXT="作者杂谈" POSITION="left" ID="ID_710781027" CREATED="1651732900155" MODIFIED="1651732924096">
+<node TEXT="实现物理过程的反过程都是重大创新" ID="ID_1778333462" CREATED="1651732631934" MODIFIED="1651732681301"/>
+<node TEXT="科技界有人把已知现象/过程重新命名, 又开了新的领域" ID="ID_597383838" CREATED="1651732930262" MODIFIED="1651732984917">
+<node TEXT="光学中的例子:&#xa;自捕获(self-trapping) -&gt; 空间孤子(spatial soliton)&#xa;光受激散射 -&gt; 二波混频 ......." ID="ID_1038920533" CREATED="1651732987315" MODIFIED="1651733085002"/>
+</node>
+</node>
+</node>
+</map>
