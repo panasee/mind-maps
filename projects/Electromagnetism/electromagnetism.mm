26770b6ab0a0b1a34d858f6bb0ecb9629917d90b
panasee <49427309+panasee@users.noreply.github.com>
2023-03-10 19:32:40 +0800

    Add spintronics and electromagnetism notes

--- projects/Electromagnetism/electromagnetism.mm
+++ projects/Electromagnetism/electromagnetism.mm
@@ -1,22 +1,28 @@
-<map version="docear 1.1" dcr_id="1548808546255_e7q58ta0o82e486l1i7xgtkkj" project="1689A0C2ACC38F80L9J3PH1B0G57P4XPHHPM">
-<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
-<node TEXT="electromagnetism" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1548808557425"><hook NAME="MapStyle">
-    <properties show_note_icons="true"/>
+<map version="freeplane 1.9.13" dcr_id="1548808546255_e7q58ta0o82e486l1i7xgtkkj" project="1689A0C2ACC38F80L9J3PH1B0G57P4XPHHPM">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="electromagnetism" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1548808557425"><hook NAME="MapStyle" zoom="1.69">
+    <properties show_note_icons="true" fit_to_viewport="false" edgeColorConfiguration="#808080ff,#808080ff,#808080ff,#808080ff,#808080ff"/>
 
 <map_styles>
-<stylenode LOCALIZED_TEXT="styles.root_node">
-<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
-<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_280645043" MAX_WIDTH="600 px" COLOR="#000000" STYLE="as_parent">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_280645043" STARTARROW="NONE" ENDARROW="DEFAULT"/>
 <font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.note"/>
 <stylenode LOCALIZED_TEXT="defaultstyle.floating">
 <edge STYLE="hide_edge"/>
 <cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
 </stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
 <font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
 </stylenode>
@@ -30,7 +36,7 @@
 <icon BUILTIN="yes"/>
 </stylenode>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
 <font SIZE="18"/>
 </stylenode>
@@ -50,10 +56,33 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="1"/>
-<node TEXT=" &#x78c1;&#x611f;&#x5e94;&#x5f3a;&#x5ea6;&#x589e;&#x52a0;&#x5fae;&#x529f;(&#x5206;&#x4e3a;&#x771f;&#x7a7a;&#x78c1;&#x573a;&#x80fd;&#x53d8;&#x5316;&#x548c;&#x78c1;&#x5316;&#x529f;)" POSITION="right" ID="ID_1702704945" CREATED="1548808625718" MODIFIED="1548809092012">
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="\latex 磁感应强度增加微功(分为真空磁场能变化和磁化功)\\&#xa;$\delta W=V \vec \mathscr H \cdot d \vec \mathscr B= V \mu_0 \vec \mathscr H \cdot d \vec \mathscr H + V \mu_0 \vec \mathscr H \cdot d\vec \mathscr M(-pdV)$" POSITION="right" ID="ID_1702704945" CREATED="1548808625718" MODIFIED="1678349449439">
 <edge COLOR="#ff0000"/>
-<hook EQUATION="\delta W=V \vec \mathscr H \cdot d \vec \mathscr B= V \mu_0 \vec \mathscr H \cdot d \vec \mathscr H + V \mu_0 \vec \mathscr H \cdot d\vec \mathscr M(-pdV)" NAME="plugins/latex/LatexNodeHook.properties"/>
+</node>
+<node TEXT="Magnetic materials" POSITION="left" ID="ID_376922966" CREATED="1678350255004" MODIFIED="1678350262515">
+<edge COLOR="#808080"/>
+<node TEXT="ferromagnetic" ID="ID_1011204097" CREATED="1678350618733" MODIFIED="1678353067975">
+<arrowlink DESTINATION="ID_45945725" MIDDLE_LABEL="above the Curie temperature" STARTINCLINATION="-123 pt;-24 pt;" ENDINCLINATION="-38 pt;-7.5 pt;"/>
+</node>
+<node TEXT="ferrimagnetic" ID="ID_1181577090" CREATED="1678351073380" MODIFIED="1678351250566"/>
+<node TEXT="antiferromagnetic" ID="ID_544125596" CREATED="1678350618733" MODIFIED="1678353137931">
+<arrowlink DESTINATION="ID_45945725" MIDDLE_LABEL="above Neel temperature" STARTINCLINATION="-80.5 pt;-14 pt;" ENDINCLINATION="-78.5 pt;0 pt;"/>
+</node>
+<node TEXT="paramagnetic" ID="ID_45945725" CREATED="1678350735516" MODIFIED="1678353119313">
+<node TEXT="due to the presence of unpaired electrons, so&#xa;most atoms with incompletely filled atomic orbitals are paramagnetic(with exceptions)" ID="ID_1848562332" CREATED="1678351735643" MODIFIED="1678352710080"/>
+</node>
+<node TEXT="diamagnetic" ID="ID_16787877" CREATED="1678351067838" MODIFIED="1678351071621">
+<node TEXT="consequence of Lenz&apos;s law,&#xa;a property of all materials (weak contribution)" ID="ID_1429084564" CREATED="1678352181565" MODIFIED="1678355392473"/>
+<node TEXT="if all electrons in the particle are paired, then the substance made of this particle is diamagnetic" ID="ID_1524897656" CREATED="1678352731106" MODIFIED="1678352775819"/>
+</node>
+</node>
+<node TEXT="\latex magnetic field(magnetic induction)\\&#xa;$B = \mu_0(H+M) = \mu_0(H+\chi H)$" POSITION="right" ID="ID_1276056384" CREATED="1678356067203" MODIFIED="1678356135164">
+<edge COLOR="#808080"/>
+<node TEXT="M is the magnetic susceptibility" ID="ID_1242033846" CREATED="1678356140806" MODIFIED="1678356159315"/>
+<node TEXT="magnetic hysteresis (including magnetization curve)" ID="ID_128179385" CREATED="1678356377868" MODIFIED="1678356467322">
+<hook URI="pix/StonerWohlfarthMainLoop.svg" SIZE="0.4892157" NAME="ExternalObject"/>
+</node>
 </node>
 </node>
 </map>
