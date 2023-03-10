c5b465535c161cb3bdfba9617b39fedf7a615277
panasee <49427309+panasee@users.noreply.github.com>
2023-03-10 19:32:40 +0800

    update

---
+++ projects/Electromagnetism/Spintronics.mm
@@ -0,0 +1,118 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Spintronics" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1678350193459">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8" zoom="1.69">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_nord_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="MR(magnetoresistance)" POSITION="right" ID="ID_77204501" CREATED="1678356835126" MODIFIED="1678356843531">
+<node TEXT="AMR (anisotropic)" ID="ID_195686864" CREATED="1678356844757" MODIFIED="1678356912570"/>
+<node TEXT="GMR (giant)" ID="ID_580653349" CREATED="1678356858599" MODIFIED="1678356931482">
+<node TEXT="multilayers - magnetic &amp; non-magnetic materials" ID="ID_1684522620" CREATED="1678357210904" MODIFIED="1678357338200"/>
+<node TEXT="metal superlattice" ID="ID_607741086" CREATED="1678357204538" MODIFIED="1678361351249">
+<node TEXT="composition" ID="ID_1017019151" CREATED="1678358455960" MODIFIED="1678358458926">
+<node TEXT="ferromagnetic -  transition metal in group 3" ID="ID_1569637352" CREATED="1678358473018" MODIFIED="1678358584720">
+<node TEXT="Fe, Co, Ni" ID="ID_1819598656" CREATED="1678358567416" MODIFIED="1678358629690"/>
+</node>
+<node TEXT="non-(ferro)magnetic -  conductor" ID="ID_401897001" CREATED="1678358524792" MODIFIED="1678358614176">
+<node TEXT="Cr, Cu, Ag, Au, Ru, Mo" ID="ID_695559437" CREATED="1678358631904" MODIFIED="1678358672847"/>
+</node>
+</node>
+<node TEXT="conditions" ID="ID_858796376" CREATED="1678357941172" MODIFIED="1678357944347">
+<node TEXT="\latex antiparallel spin configuration\\&#xa;* relative orientation of the magnetic moment of adjacent layers can be modulated through external magnetic field\\&#xa;* the period of superlattice is shorted than the mean free path of electrons\\&#xa;* the scattering rates of electrons with different spin polarization have significant difference" ID="ID_1719042385" CREATED="1678357944777" MODIFIED="1678361068556"/>
+</node>
+</node>
+<node TEXT="spin valve" ID="ID_575887588" CREATED="1678361353861" MODIFIED="1678361357008">
+<node TEXT="composition 4 layers&#xa;(AFM/FM/NM/FM - AFM/pinned/spacer/free layer)" ID="ID_1675883196" CREATED="1678361376954" MODIFIED="1678363515856">
+<node TEXT="pinned layer and free layer are different" ID="ID_546769185" CREATED="1678363335616" MODIFIED="1678363627171"/>
+</node>
+<node TEXT="conditions" ID="ID_750240141" CREATED="1678361839443" MODIFIED="1678361841697">
+<node TEXT="\latex antiparallel spin configuration\\&#xa;* the thickness of NM layer is shorted than the mean free path of electrons and spins\\&#xa;* the scattering rates of electrons in FM layers and on FM/NM interfaces must be spin-correlated" ID="ID_633923597" CREATED="1678357944777" MODIFIED="1678362219986"/>
+</node>
+<node TEXT="MR" ID="ID_1348727314" CREATED="1678362323181" MODIFIED="1678362330667">
+<node TEXT="resistance of SV depends on the relative orientation of the magnetic moment of 2 FM layers" ID="ID_218118608" CREATED="1678362330960" MODIFIED="1678362375979"/>
+</node>
+</node>
+</node>
+<node TEXT="CMR (colossal)" ID="ID_738075893" CREATED="1678356860819" MODIFIED="1678356939088"/>
+<node TEXT="TMR (tunnel)" ID="ID_854809781" CREATED="1678356865044" MODIFIED="1678356944711">
+<node TEXT="structure is similar to GMR SV, but replace spacer layer with insulator&#xa;(electrons can only pass through by tunnelling)" ID="ID_651086564" CREATED="1678363404605" MODIFIED="1678363901608"/>
+</node>
+<node TEXT="EMR (extrodinary)" ID="ID_189892240" CREATED="1678357095201" MODIFIED="1678357103197"/>
+</node>
+</node>
+</map>
