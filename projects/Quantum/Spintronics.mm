b55f6d328798776fcf4479f8cc95a8eca90e28f0
panasee <49427309+panasee@users.noreply.github.com>
2023-08-18 21:11:54 +0800

    create spintronics

---
+++ projects/Quantum/Spintronics.mm
@@ -0,0 +1,102 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Spintronics" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1692354177196" STYLE="oval">
+<font SIZE="22"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" associatedTemplateLocation="template:/standard-1.6.mm" followedTemplateLocation="template:/standard-1.6.mm" followedMapLastTime="1690525622000" show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#000000" STYLE="fork" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" CHILD_NODES_LAYOUT="AUTO" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#808080" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="Times New Roman" SIZE="14" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<edge COLOR="#808080"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Magnetism" POSITION="bottom_or_right" ID="ID_1369141927" CREATED="1692353485174" MODIFIED="1692354169092">
+<edge COLOR="#ff0000"/>
+<font NAME="Times New Roman" SIZE="14"/>
+<node TEXT="Hallmark" ID="ID_1995791046" CREATED="1692353653941" MODIFIED="1692354809069">
+<font NAME="Times New Roman" SIZE="14"/>
+<node TEXT="ordered arrangement of magnetic moments over macroscopic length scales" ID="ID_764955193" CREATED="1692354809280" MODIFIED="1692354819516"/>
+<node TEXT="spontaneous breaking of time-reversal symmetry" ID="ID_1482073654" CREATED="1692354822373" MODIFIED="1692354825755"/>
+</node>
+<node TEXT="driving force: exchange coupling" ID="ID_410544439" CREATED="1692353996738" MODIFIED="1692354169076">
+<font NAME="Times New Roman" SIZE="14"/>
+</node>
+<node TEXT="Curie Temp" LOCALIZED_STYLE_REF="default" ID="ID_648910179" CREATED="1692354213685" MODIFIED="1692354788944">
+<font NAME="Times New Roman" SIZE="14"/>
+<node TEXT="thermal fluctuations misalign magnetic moments in neighboring regions" ID="ID_608301667" CREATED="1692354789435" MODIFIED="1692354792436"/>
+</node>
+<node TEXT="Dimensionality" ID="ID_522920018" CREATED="1692354575970" MODIFIED="1692354585510">
+<node TEXT="essential role in determining the impact of thermal fluctuations" ID="ID_1661138496" CREATED="1692354742322" MODIFIED="1692354780004"/>
+<node TEXT="in 3D" ID="ID_277507914" CREATED="1692355226998" MODIFIED="1692355235842">
+<node TEXT="magnetic phase transition always occur at a finite temp" ID="ID_1950707613" CREATED="1692355244796" MODIFIED="1692355289116"/>
+</node>
+<node TEXT="in 2D" ID="ID_988964255" CREATED="1692355237294" MODIFIED="1692355240126">
+<node TEXT="complex situation" ID="ID_521517545" CREATED="1692355346181" MODIFIED="1692355349982"/>
+</node>
+<node TEXT="in 1D" ID="ID_171660917" CREATED="1692355240458" MODIFIED="1692355242694">
+<node TEXT="long range order possible only @ T=0" ID="ID_1812797049" CREATED="1692355307597" MODIFIED="1692355321092"/>
+</node>
+</node>
+</node>
+</node>
+</map>
