9ea063df410734fb7b727c579d1a8f4f35329f69
panasee <49427309+panasee@users.noreply.github.com>
2023-09-02 20:54:18 +0800

    update

--- projects/Electromagnetism/electromagnetism.mm
+++ projects/Electromagnetism/electromagnetism.mm
@@ -1,28 +1,44 @@
-<map version="freeplane 1.9.13" dcr_id="1548808546255_e7q58ta0o82e486l1i7xgtkkj" project="1689A0C2ACC38F80L9J3PH1B0G57P4XPHHPM">
+<map version="freeplane 1.11.5" dcr_id="1548808546255_e7q58ta0o82e486l1i7xgtkkj" project="1689A0C2ACC38F80L9J3PH1B0G57P4XPHHPM">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
-<node TEXT="electromagnetism" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1548808557425"><hook NAME="MapStyle" zoom="1.69">
-    <properties show_note_icons="true" fit_to_viewport="false" edgeColorConfiguration="#808080ff,#808080ff,#808080ff,#808080ff,#808080ff"/>
+<node TEXT="electromagnetism" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1693646221102">
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="MapStyle" background="#f9f9f8" zoom="1.001">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" show_icon_for_attributes="true"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
 <font SIZE="24"/>
-<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
-<stylenode LOCALIZED_TEXT="default" ID="ID_280645043" MAX_WIDTH="600 px" COLOR="#000000" STYLE="as_parent">
-<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_280645043" STARTARROW="NONE" ENDARROW="DEFAULT"/>
-<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.attributes">
-<font SIZE="9"/>
+<font SIZE="10"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.floating">
 <edge STYLE="hide_edge"/>
 <cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
 <stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
 <font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
 </stylenode>
@@ -32,42 +48,57 @@
 <stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
 <font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.important">
-<icon BUILTIN="yes"/>
 </stylenode>
-</stylenode>
-<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
 <font SIZE="18"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
 <font SIZE="16"/>
+<edge COLOR="#ff0000"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
 <font SIZE="14"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
 <font SIZE="12"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
 <font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
 </stylenode>
 </stylenode>
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
-<node TEXT="\latex 磁感应强度增加微功(分为真空磁场能变化和磁化功)\\&#xa;$\delta W=V \vec \mathscr H \cdot d \vec \mathscr B= V \mu_0 \vec \mathscr H \cdot d \vec \mathscr H + V \mu_0 \vec \mathscr H \cdot d\vec \mathscr M(-pdV)$" POSITION="right" ID="ID_1702704945" CREATED="1548808625718" MODIFIED="1678349449439">
-<edge COLOR="#ff0000"/>
-</node>
-<node TEXT="Magnetic materials" POSITION="left" ID="ID_376922966" CREATED="1678350255004" MODIFIED="1678350262515">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font SIZE="24"/>
+<node TEXT="\latex 磁感应强度增加微功\\(分为真空磁场能变化和磁化功)\\&#xa;$\delta W=V \vec \mathscr H \cdot d \vec \mathscr B\\= V \mu_0 \vec \mathscr H \cdot d \vec \mathscr H + V \mu_0 \vec \mathscr H \cdot d\vec \mathscr M(-pdV)$" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="bottom_or_right" ID="ID_1702704945" CREATED="1548808625718" MODIFIED="1693646333854"/>
+<node TEXT="Magnetic materials" POSITION="top_or_left" ID="ID_376922966" CREATED="1678350255004" MODIFIED="1678350262515">
 <edge COLOR="#808080"/>
 <node TEXT="ferromagnetic" ID="ID_1011204097" CREATED="1678350618733" MODIFIED="1678353067975">
-<arrowlink DESTINATION="ID_45945725" MIDDLE_LABEL="above the Curie temperature" STARTINCLINATION="-123 pt;-24 pt;" ENDINCLINATION="-38 pt;-7.5 pt;"/>
+<arrowlink DESTINATION="ID_45945725" MIDDLE_LABEL="above the Curie temperature" STARTINCLINATION="-122.4 pt;-23.4 pt;" ENDINCLINATION="-37.2 pt;-6.6 pt;"/>
 </node>
 <node TEXT="ferrimagnetic" ID="ID_1181577090" CREATED="1678351073380" MODIFIED="1678351250566"/>
 <node TEXT="antiferromagnetic" ID="ID_544125596" CREATED="1678350618733" MODIFIED="1678353137931">
-<arrowlink DESTINATION="ID_45945725" MIDDLE_LABEL="above Neel temperature" STARTINCLINATION="-80.5 pt;-14 pt;" ENDINCLINATION="-78.5 pt;0 pt;"/>
+<arrowlink DESTINATION="ID_45945725" MIDDLE_LABEL="above Neel temperature" STARTINCLINATION="-79.8 pt;-13.2 pt;" ENDINCLINATION="-78 pt;0 pt;"/>
 </node>
 <node TEXT="paramagnetic" ID="ID_45945725" CREATED="1678350735516" MODIFIED="1678353119313">
 <node TEXT="due to the presence of unpaired electrons, so&#xa;most atoms with incompletely filled atomic orbitals are paramagnetic(with exceptions)" ID="ID_1848562332" CREATED="1678351735643" MODIFIED="1678352710080"/>
@@ -77,11 +108,39 @@
 <node TEXT="if all electrons in the particle are paired, then the substance made of this particle is diamagnetic" ID="ID_1524897656" CREATED="1678352731106" MODIFIED="1678352775819"/>
 </node>
 </node>
-<node TEXT="\latex magnetic field(magnetic induction)\\&#xa;$B = \mu_0(H+M) = \mu_0(H+\chi H)$" POSITION="right" ID="ID_1276056384" CREATED="1678356067203" MODIFIED="1678356135164">
+<node TEXT="\latex magnetic field(magnetic induction)\\&#xa;$B = \mu_0(H+M) = \mu_0(H+\chi H)$" POSITION="bottom_or_right" ID="ID_1276056384" CREATED="1678356067203" MODIFIED="1678356135164">
 <edge COLOR="#808080"/>
-<node TEXT="M is the magnetic susceptibility" ID="ID_1242033846" CREATED="1678356140806" MODIFIED="1678356159315"/>
+<node TEXT="\latex \chi is the magnetic susceptibility" ID="ID_1242033846" CREATED="1678356140806" MODIFIED="1693644284467"/>
 <node TEXT="magnetic hysteresis (including magnetization curve)" ID="ID_128179385" CREATED="1678356377868" MODIFIED="1678356467322">
 <hook URI="pix/StonerWohlfarthMainLoop.svg" SIZE="0.4892157" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="\latex permittivity \epsilon and permeability \mu" POSITION="top_or_left" ID="ID_799966376" CREATED="1693644305086" MODIFIED="1693644337322">
+<edge COLOR="#808080"/>
+<node TEXT="dimension" ID="ID_1307507522" CREATED="1693644389890" MODIFIED="1693644392617">
+<node TEXT="\latex $[\epsilon] =[M^{-1}L^{-3}T^4I^2]=F/m$" ID="ID_962427122" CREATED="1693644395506" MODIFIED="1693644638480"/>
+<node TEXT="\latex $[\mu]=[M^1L^1T^{-2}I^{-2}]=H/m$" ID="ID_1276975996" CREATED="1693644455628" MODIFIED="1693644510118"/>
+<node TEXT="common basic formulas" ID="ID_765167969" CREATED="1693644762090" MODIFIED="1693649322885">
+<node TEXT="\latex $F=IBL$" ID="ID_1446049289" CREATED="1693644773975" MODIFIED="1693644788350"/>
+<node TEXT="\latex Amp\` ere&apos;s force: $\frac{F_m}L=2k_A\frac{I_1I_2}{r}, (k_A \overset\mathrm{def}=\frac{\mu_0}{4\pi})$" ID="ID_1429118608" CREATED="1693644820016" MODIFIED="1693645440943"/>
+<node TEXT="\latex Lorentz force: $\vec F = q(\vec E+\vec v\times \vec B)$" ID="ID_335620057" CREATED="1693645425990" MODIFIED="1693645501850">
+<node TEXT="\latex $T=[B]=[M^1 I^{-1} T^{-2}]=kg /A /s^2 $" ID="ID_1316203598" CREATED="1693645759335" MODIFIED="1693645917859"/>
+</node>
+<node TEXT="\latex Biot-Savart law: ${\displaystyle \mathbf {B} (\mathbf {r} )={\frac {\mu _{0}}{4\pi }}\int _{C}{\frac {I\,d{\boldsymbol {\ell }}\times \mathbf {r&apos;} }{|\mathbf {r&apos;} |^{3}}}}$" ID="ID_1942652099" CREATED="1693648519162" MODIFIED="1693648573916"/>
+<node TEXT="\latex Magnetic Flux: $\Phi_B=\iint_S\vec B\cdot d\vec S=\Phi_B=\oint_{\partial S}\vec A\cdot d\vec l$" ID="ID_626867212" CREATED="1693648819303" MODIFIED="1693648992796"/>
+<node TEXT="\latex Inductance: $v(t)=\mathcal L\frac{di}{dt}$" ID="ID_435141283" CREATED="1693649658675" MODIFIED="1693652259671">
+<node TEXT="\latex $H=[\mathcal L]=[M^1L^2T^{-2}I^{-2}]$" ID="ID_1631841167" CREATED="1693652161654" MODIFIED="1693652233302"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Maxwell Equations" POSITION="bottom_or_right" ID="ID_1880520007" CREATED="1693652490474" MODIFIED="1693652499945">
+<edge COLOR="#00ffff"/>
+<node TEXT="Differential Equations" ID="ID_1442361923" CREATED="1693652502613" MODIFIED="1693652518974">
+<node TEXT="\latex Gauss&apos;s law: $\nabla\cdot \vec E=\frac{\rho}{\epsilon_0}$" ID="ID_459256612" CREATED="1693652520643" MODIFIED="1693652695555"/>
+<node TEXT="\latex Gauss&apos;s law for magnetism: $\nabla \cdot \vec B = 0$" ID="ID_352709091" CREATED="1693652545318" MODIFIED="1693652963590"/>
+<node TEXT="\latex Maxwell-Faraday equation: $\nabla \times \vec E=-\frac{\partial \vec B}{\partial t}$" ID="ID_1684613503" CREATED="1693652560657" MODIFIED="1693653088959"/>
+<node TEXT="\latex Amp\`ere&apos;s circuital law: $\nabla\times\vec B = \mu_0 (\vec J+\epsilon_0 \frac{\partial \vec E}{\partial t})$" ID="ID_1463445194" CREATED="1693652575803" MODIFIED="1693653171580"/>
 </node>
 </node>
 </node>
--- projects/Quantum/Spintronics.mm
+++ projects/Quantum/Spintronics.mm
@@ -1,33 +1,46 @@
 <map version="freeplane 1.11.5">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
-<node TEXT="Spintronics" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1692354177196" STYLE="oval">
+<node TEXT="Spintronics" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1693646037716" STYLE="oval">
+<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
 <font SIZE="22"/>
-<hook NAME="MapStyle">
-    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" associatedTemplateLocation="template:/standard-1.6.mm" show_note_icons="true" followedTemplateLocation="template:/standard-1.6.mm" followedMapLastTime="1690525622000" fit_to_viewport="false"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#f9f9f8" zoom="1.001">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" show_note_icons="true" followedTemplateLocation="template:/standard-1.6.mm" followedMapLastTime="1692705696000" fit_to_viewport="false"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
 <font SIZE="24"/>
 <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
-<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#000000" STYLE="fork" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" CHILD_NODES_LAYOUT="AUTO" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#808080" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="SOLID">
-<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
-<font NAME="Times New Roman" SIZE="14" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
 <richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
 <richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
-<edge COLOR="#808080"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.attributes">
-<font SIZE="9"/>
+<font SIZE="10"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.floating">
 <edge STYLE="hide_edge"/>
 <cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
 <stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
 <font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
 </stylenode>
@@ -37,39 +50,46 @@
 <stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
 <font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
-<icon BUILTIN="yes"/>
-<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
-</stylenode>
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
 <font SIZE="18"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<edge COLOR="#ff0000"/>
 <font SIZE="16"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
 <font SIZE="14"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
 <font SIZE="12"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
 <font SIZE="10"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
 </stylenode>
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
 <node TEXT="Magnetism" POSITION="bottom_or_right" ID="ID_1369141927" CREATED="1692353485174" MODIFIED="1692354169092">
 <edge COLOR="#ff0000"/>
 <font NAME="Times New Roman" SIZE="14"/>
@@ -81,8 +101,8 @@
 <node TEXT="driving force: exchange coupling" ID="ID_410544439" CREATED="1692353996738" MODIFIED="1692354169076">
 <font NAME="Times New Roman" SIZE="14"/>
 </node>
-<node TEXT="Curie Temp" LOCALIZED_STYLE_REF="default" ID="ID_648910179" CREATED="1692354213685" MODIFIED="1692354788944">
-<font NAME="Times New Roman" SIZE="14"/>
+<node TEXT="Curie Temp" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_648910179" CREATED="1692354213685" MODIFIED="1693646096840">
+<font NAME="Times New Roman"/>
 <node TEXT="thermal fluctuations misalign magnetic moments in neighboring regions" ID="ID_608301667" CREATED="1692354789435" MODIFIED="1692354792436"/>
 </node>
 <node TEXT="Dimensionality" ID="ID_522920018" CREATED="1692354575970" MODIFIED="1692354585510">
@@ -100,6 +120,42 @@
 </node>
 <node TEXT="Spin-Orbit Coupling" POSITION="top_or_left" ID="ID_291027025" CREATED="1693384944936" MODIFIED="1693384951240">
 <edge COLOR="#00ff00"/>
+</node>
+<node TEXT="MR(magnetoresistance)" POSITION="bottom_or_right" ID="ID_77204501" CREATED="1678356835126" MODIFIED="1693645981574">
+<edge COLOR="#ff00ff"/>
+<node TEXT="AMR (anisotropic)" ID="ID_195686864" CREATED="1678356844757" MODIFIED="1678356912570"/>
+<node TEXT="GMR (giant)" ID="ID_580653349" CREATED="1678356858599" MODIFIED="1678356931482">
+<node TEXT="multilayers - magnetic &amp; non-magnetic materials" ID="ID_1684522620" CREATED="1678357210904" MODIFIED="1678357338200"/>
+<node TEXT="metal superlattice" ID="ID_607741086" CREATED="1678357204538" MODIFIED="1678361351249">
+<node TEXT="composition" ID="ID_1017019151" CREATED="1678358455960" MODIFIED="1678358458926">
+<node TEXT="ferromagnetic -  transition metal in group 3" ID="ID_1569637352" CREATED="1678358473018" MODIFIED="1678358584720">
+<node TEXT="Fe, Co, Ni" ID="ID_1819598656" CREATED="1678358567416" MODIFIED="1678358629690"/>
+</node>
+<node TEXT="non-(ferro)magnetic -  conductor" ID="ID_401897001" CREATED="1678358524792" MODIFIED="1678358614176">
+<node TEXT="Cr, Cu, Ag, Au, Ru, Mo" ID="ID_695559437" CREATED="1678358631904" MODIFIED="1678358672847"/>
+</node>
+</node>
+<node TEXT="conditions" ID="ID_858796376" CREATED="1678357941172" MODIFIED="1678357944347">
+<node TEXT="\latex antiparallel spin configuration\\&#xa;* relative orientation of the magnetic moment of adjacent layers can be modulated through external magnetic field\\&#xa;* the period of superlattice is shorted than the mean free path of electrons\\&#xa;* the scattering rates of electrons with different spin polarization have significant difference" ID="ID_1719042385" CREATED="1678357944777" MODIFIED="1678361068556"/>
+</node>
+</node>
+<node TEXT="spin valve" ID="ID_575887588" CREATED="1678361353861" MODIFIED="1678361357008">
+<node TEXT="composition 4 layers&#xa;(AFM/FM/NM/FM - AFM/pinned/spacer/free layer)" ID="ID_1675883196" CREATED="1678361376954" MODIFIED="1678363515856">
+<node TEXT="pinned layer and free layer are different" ID="ID_546769185" CREATED="1678363335616" MODIFIED="1678363627171"/>
+</node>
+<node TEXT="conditions" ID="ID_750240141" CREATED="1678361839443" MODIFIED="1678361841697">
+<node TEXT="\latex antiparallel spin configuration\\&#xa;* the thickness of NM layer is shorted than the mean free path of electrons and spins\\&#xa;* the scattering rates of electrons in FM layers and on FM/NM interfaces must be spin-correlated" ID="ID_633923597" CREATED="1678357944777" MODIFIED="1678362219986"/>
+</node>
+<node TEXT="MR" ID="ID_1348727314" CREATED="1678362323181" MODIFIED="1678362330667">
+<node TEXT="resistance of SV depends on the relative orientation of the magnetic moment of 2 FM layers" ID="ID_218118608" CREATED="1678362330960" MODIFIED="1678362375979"/>
+</node>
+</node>
+</node>
+<node TEXT="CMR (colossal)" ID="ID_738075893" CREATED="1678356860819" MODIFIED="1678356939088"/>
+<node TEXT="TMR (tunnel)" ID="ID_854809781" CREATED="1678356865044" MODIFIED="1678356944711">
+<node TEXT="structure is similar to GMR SV, but replace spacer layer with insulator&#xa;(electrons can only pass through by tunnelling)" ID="ID_651086564" CREATED="1678363404605" MODIFIED="1678363901608"/>
+</node>
+<node TEXT="EMR (extrodinary)" ID="ID_189892240" CREATED="1678357095201" MODIFIED="1678357103197"/>
 </node>
 </node>
 </map>
