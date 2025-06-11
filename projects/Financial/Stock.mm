c4f8d089c96a6b5144535d796d2188b5895a7d63
panasee <49427309+panasee@users.noreply.github.com>
2025-06-12 01:36:28 +0800

    update Financial

---
+++ projects/Financial/Stock.mm
@@ -0,0 +1,251 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="股票市场" FOLDED="false" ID="ID_452789050" CREATED="1603077282276" MODIFIED="1749651230277" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.4641001">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" show_tags="UNDER_NODES" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_791056231" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_791056231" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="16" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="K线" POSITION="top_or_left" ID="ID_1529295653" CREATED="1749653280098" MODIFIED="1749653516445">
+<edge COLOR="#007c7c"/>
+<node TEXT="主要K线和次要K线" ID="ID_249765470" CREATED="1749653522992" MODIFIED="1749653535145">
+<node TEXT="倍量K线且涨幅较大(5%)的为主要K线；&#xa;一根主要K线能决定至少三根次要K线" ID="ID_1590773034" CREATED="1749653536002" MODIFIED="1749653593320"/>
+</node>
+<node TEXT="支撑位和压力位" ID="ID_196547995" CREATED="1749658612813" MODIFIED="1749658617359">
+<node TEXT="缠论中枢（静态）" ID="ID_1959893229" CREATED="1749658618205" MODIFIED="1749658727891">
+<node TEXT="基于价格行为的静态关键位" ID="ID_1793324084" CREATED="1749658747282" MODIFIED="1749658764855">
+<node TEXT="锚定效应：特定价格成为心理关口" ID="ID_615740303" CREATED="1749659055238" MODIFIED="1749659070216">
+<node TEXT="历史高低点、密集成交区" POSITION="top_or_left" ID="ID_67509289" CREATED="1749659083379" MODIFIED="1749659091568">
+<node TEXT="群体记忆形成自证预言" ID="ID_2566274" CREATED="1749659107461" MODIFIED="1749659115090"/>
+</node>
+<node TEXT="价格多次在区间反复震荡，&#xa;该区间会成为交易者心中价值锚点" POSITION="top_or_left" ID="ID_1493402804" CREATED="1749659354621" MODIFIED="1749659392914"/>
+</node>
+<node TEXT="后悔厌恶：套牢盘在成本区卖出(压力位)，&#xa;踏空者在回落后补仓(支撑位)" POSITION="top_or_left" ID="ID_56137852" CREATED="1749659395646" MODIFIED="1749659459269"/>
+<node TEXT="自我实现预言： 触及价位时大规模成交，&#xa;放大支撑/压力效果" POSITION="top_or_left" ID="ID_1018091699" CREATED="1749659521172" MODIFIED="1749659547834"/>
+<node TEXT="窄幅区间焦点效应：&#xa;价格临近时，市场注意力集中，&#xa;加剧波动，造成放量突破/假突破" POSITION="top_or_left" ID="ID_503192384" CREATED="1749659596703" MODIFIED="1749659638231"/>
+</node>
+</node>
+<node TEXT="均线（动态）" ID="ID_697019154" CREATED="1749658622374" MODIFIED="1749658731127">
+<node TEXT="基于时间序列的动态指标" ID="ID_1117736939" CREATED="1749658767726" MODIFIED="1749658775030">
+<node TEXT="不同均线吸引不同交易风格的群体，&#xa;本质上是动态共识平台" ID="ID_1534085124" CREATED="1749659123084" MODIFIED="1749659191617">
+<node TEXT="5日线-短线客" ID="ID_968873829" CREATED="1749659136085" MODIFIED="1749659153331"/>
+<node TEXT="50日线-机构调仓" ID="ID_679487614" CREATED="1749659154192" MODIFIED="1749659165876"/>
+<node TEXT="200日线-大资金牛熊分界" ID="ID_495478464" CREATED="1749659167256" MODIFIED="1749659176497"/>
+</node>
+<node TEXT="价格运行到均线附近，引发博弈" ID="ID_63557276" CREATED="1749659215012" MODIFIED="1749659238078"/>
+<node TEXT="均线代表一段时间内的平均持仓成本" ID="ID_286805608" CREATED="1749659757753" MODIFIED="1749659778932">
+<node TEXT="高于均线，多数人浮盈，心态稳定，&#xa;回调倾向加仓，支撑" ID="ID_1221314160" CREATED="1749659783504" MODIFIED="1749659811472"/>
+<node TEXT="低于均线，多数人浮亏，&#xa;恐慌抛售引发踩踏，压力" ID="ID_779838761" CREATED="1749659813450" MODIFIED="1749659852809"/>
+</node>
+</node>
+<node TEXT="多重均线密集交织，形成跨周期心理共振点，&#xa;力量远超单根均线" ID="ID_621224242" CREATED="1749660009758" MODIFIED="1749660048088"/>
+<node TEXT="趋势验证：回踩支撑，反弹压力位诱多" ID="ID_1690723510" CREATED="1749660073378" MODIFIED="1749660098986"/>
+</node>
+<node TEXT="结合策略" ID="ID_12631659" CREATED="1749659252724" MODIFIED="1749659256431">
+<node TEXT="用中枢确定战略支撑区，用均线捕捉战术入场点" ID="ID_1023304768" CREATED="1749659257062" MODIFIED="1749659275708"/>
+</node>
+</node>
+</node>
+<node TEXT="超短线" POSITION="bottom_or_right" ID="ID_1086520389" CREATED="1749651279955" MODIFIED="1749653317715">
+<edge COLOR="#7c7c00"/>
+<node TEXT="A股涨停板" ID="ID_1636247497" CREATED="1749653380074" MODIFIED="1749653389281">
+<node TEXT="选股" POSITION="bottom_or_right" ID="ID_1567560148" CREATED="1749651616643" MODIFIED="1749651619977">
+<node TEXT="热门题材" ID="ID_1186071835" CREATED="1749651673446" MODIFIED="1749651676700">
+<node TEXT="板块至少两个一进二" ID="ID_787903891" CREATED="1749651762901" MODIFIED="1749651780174"/>
+</node>
+<node TEXT="龙头股" ID="ID_682024483" CREATED="1749651718011" MODIFIED="1749651721598">
+<node TEXT="涨停时间越早说明资金越青睐&#xa;十点之前为重点关注时段" ID="ID_1973689185" CREATED="1749654477968" MODIFIED="1749654579244"/>
+</node>
+</node>
+<node TEXT="时机" POSITION="bottom_or_right" ID="ID_1159573561" CREATED="1749651621590" MODIFIED="1749651624161">
+<node ID="ID_1412305085" CREATED="1749652451229" MODIFIED="1749652795854"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      第一个涨停后回调<span style="font-weight: bold;">缩量</span>且<span style="font-weight: bold;">不破支撑</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+<hook URI="1.png" SIZE="0.283848" NAME="ExternalObject"/>
+</node>
+<node TEXT="尾盘涨停需谨慎" ID="ID_1648300136" CREATED="1749652962218" MODIFIED="1749652969466"/>
+<node TEXT="板数特征" ID="ID_795110130" CREATED="1749654590221" MODIFIED="1749654595574">
+<node TEXT="一板无法判断持续性，若打一板必须&#xa;主流板块或者强势板块前排个股" POSITION="bottom_or_right" ID="ID_1514807873" CREATED="1749654396638" MODIFIED="1749654448640">
+<node TEXT="如果一字板进不去就&#xa;打早盘涨停股票" ID="ID_1626786976" CREATED="1749654451885" MODIFIED="1749654499805">
+<node TEXT="前十大股东情况" ID="ID_1770655087" CREATED="1749654500120" MODIFIED="1749654508119"/>
+<node TEXT="筹码结构" ID="ID_1818749188" CREATED="1749654508720" MODIFIED="1749654511444"/>
+<node TEXT="股价" ID="ID_1607132076" CREATED="1749654511777" MODIFIED="1749654513815"/>
+<node TEXT="位置" ID="ID_1103448616" CREATED="1749654513961" MODIFIED="1749654515538"/>
+</node>
+</node>
+<node TEXT="二板是题材发酵板，大致判断持续性" POSITION="bottom_or_right" ID="ID_1070301552" CREATED="1749654601539" MODIFIED="1749654623498">
+<node TEXT="至少三个二板说明板块持续性" ID="ID_220949431" CREATED="1749654631552" MODIFIED="1749654647868"/>
+<node ID="ID_379201853" CREATED="1749654709058" MODIFIED="1749654719934"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      二板股是<span style="font-weight: bold;">龙头种子股</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="策略：打最先涨停的二连板，不要在有二板时去打一板" ID="ID_1137339820" CREATED="1749654665938" MODIFIED="1749654694744"/>
+</node>
+<node TEXT="三板是竞争龙头分水岭" POSITION="bottom_or_right" ID="ID_165495048" CREATED="1749654746249" MODIFIED="1749654756933">
+<node TEXT="策略：加仓或者打最先涨停的三板；&#xa;若进不去，围绕龙头对小弟套利，&#xa;做日内卡位股和最强前排助攻股" ID="ID_401160292" CREATED="1749654774295" MODIFIED="1749654846117">
+<node TEXT="套利不可逗留，及时卖出" ID="ID_1316252982" CREATED="1749654955454" MODIFIED="1749654963853"/>
+</node>
+<node TEXT="一般从集合竞价高开情况就能判断二升三" ID="ID_719373022" CREATED="1749654867571" MODIFIED="1749654890592"/>
+</node>
+<node TEXT="四到五板常常是热点顶部" POSITION="bottom_or_right" ID="ID_191131974" CREATED="1749654988979" MODIFIED="1749655002353">
+<node TEXT="如果热点级别不够大，则四到五板就会是顶部" ID="ID_1601044594" CREATED="1749655006160" MODIFIED="1749655028067"/>
+<node TEXT="热点级别" ID="ID_812576155" CREATED="1749655034141" MODIFIED="1749655039764">
+<node TEXT="资金容量" ID="ID_524700208" CREATED="1749655040843" MODIFIED="1749655044438"/>
+<node TEXT="持续利好消息" ID="ID_512341067" CREATED="1749655044934" MODIFIED="1749655049793"/>
+<node TEXT="判断会不会继续发酵" ID="ID_216509792" CREATED="1749655051514" MODIFIED="1749655058673"/>
+</node>
+<node TEXT="策略：继续龙头或者日内卡位股/最强前排助攻股套利" ID="ID_1848347232" CREATED="1749655066931" MODIFIED="1749655094992"/>
+</node>
+</node>
+</node>
+<node TEXT="交易" POSITION="bottom_or_right" ID="ID_1664513621" CREATED="1749651628055" MODIFIED="1749651630205">
+<node TEXT="小资金试板" ID="ID_1527189813" CREATED="1749652998693" MODIFIED="1749653026384">
+<node TEXT="板上封单主力可能会采用不断撤旧单加新单的方式出货（这样散户的买单会被推到最前优先成交）" ID="ID_337131582" CREATED="1749653018167" MODIFIED="1749653113348"/>
+<node TEXT="板上挂一手如果很快成交说明在出货，需要止盈" ID="ID_1751138382" CREATED="1749653172790" MODIFIED="1749653221625"/>
+</node>
+<node TEXT="止损" ID="ID_508772241" CREATED="1749655281367" MODIFIED="1749655635982">
+<node TEXT="如果外围股对应板块涨停数不达预期，或者出现一进二和二进三失败（三天不板或者破支撑），后一天及时卖出，不做逗留" ID="ID_667210327" CREATED="1749655284146" MODIFIED="1749655761446">
+<hook URI="2.png" SIZE="0.59937173" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="资金流向" POSITION="bottom_or_right" ID="ID_715513396" CREATED="1749653683473" MODIFIED="1749653691473">
+<edge COLOR="#ff0000"/>
+<node TEXT="大单" ID="ID_914956818" CREATED="1749653691903" MODIFIED="1749653696862">
+<node TEXT="连续至少6笔买入/卖出大单&#xa;(无反向单打断，说明主力买入/卖出)" ID="ID_1516530992" CREATED="1749653697190" MODIFIED="1749654023191">
+<node TEXT="\latex 对于市值x亿，大单标准(万)\\&#xa;$\left\{\begin{matrix}100, 2x&lt;100\\&#xa;2x, x&lt;300\end{matrix}\right.$" ID="ID_1965073815" CREATED="1749653714607" MODIFIED="1749653885732"/>
+</node>
+</node>
+</node>
+<node TEXT="基本面" POSITION="top_or_left" ID="ID_813771538" CREATED="1749654280798" MODIFIED="1749654285888">
+<edge COLOR="#0000ff"/>
+<node TEXT="市值分析" ID="ID_1373426903" CREATED="1749654300676" MODIFIED="1749654308619">
+<node TEXT="PE" ID="ID_1811089900" CREATED="1749654311704" MODIFIED="1749654313941"/>
+<node TEXT="PS" ID="ID_578595924" CREATED="1749656560990" MODIFIED="1749656562132"/>
+<node TEXT="PB" ID="ID_1854768713" CREATED="1749654314433" MODIFIED="1749654315837"/>
+<node TEXT="估值" ID="ID_617121472" CREATED="1749656660284" MODIFIED="1749656663836">
+<node TEXT="现金流折现模型（DCF）" ID="ID_1651584634" CREATED="1749656666320" MODIFIED="1749656674868"/>
+</node>
+</node>
+<node TEXT="财务指标" ID="ID_661469848" CREATED="1749654288885" MODIFIED="1749654292431">
+<node TEXT="应收账款周转率和周转天数" ID="ID_522976281" CREATED="1749654353586" MODIFIED="1749654369628"/>
+</node>
+<node TEXT="PEG比率（市盈率/预期盈利增长率）" ID="ID_1854425473" CREATED="1749656609113" MODIFIED="1749656625458"/>
+<node TEXT="预期回报率" ID="ID_411870913" CREATED="1749656684216" MODIFIED="1749656688725"/>
+</node>
+<node TEXT="对冲和止损" POSITION="bottom_or_right" ID="ID_1169356412" CREATED="1749656831954" MODIFIED="1749656836334">
+<edge COLOR="#ff00ff"/>
+<node TEXT="对冲" ID="ID_635912211" CREATED="1749656932957" MODIFIED="1749656935660">
+<node TEXT="买入put" ID="ID_1504798461" CREATED="1749656935993" MODIFIED="1749656953176"/>
+<node TEXT="卖出call" ID="ID_588530429" CREATED="1749656953772" MODIFIED="1749656956773"/>
+</node>
+<node TEXT="止损" ID="ID_454687424" CREATED="1749656972775" MODIFIED="1749656975323">
+<node TEXT="远离密集成交区，下浮3-5%或者参考ATR" ID="ID_1187124014" CREATED="1749656975680" MODIFIED="1749657094050"/>
+<node TEXT="动态跟踪止损，VIX飙升时临时扩大止损空间，&#xa;避免过早错误止损" ID="ID_746362070" CREATED="1749657053023" MODIFIED="1749657147866"/>
+<node TEXT="分批止损（金字塔减仓）" ID="ID_1154634542" CREATED="1749657152068" MODIFIED="1749657174640">
+<node TEXT="划分多个支撑位，依次分批减仓" ID="ID_974957613" CREATED="1749657183232" MODIFIED="1749657216582"/>
+</node>
+<node TEXT="时间止损法" ID="ID_1135105037" CREATED="1749657245784" MODIFIED="1749657254692">
+<node TEXT="破位后设定观察窗口期" ID="ID_1562823625" CREATED="1749657255063" MODIFIED="1749657267097">
+<node TEXT="常用3根K线" ID="ID_1147995450" CREATED="1749657311318" MODIFIED="1749657318615"/>
+<node TEXT="当日收盘" ID="ID_285367550" CREATED="1749657340533" MODIFIED="1749657367736"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="风险管理" POSITION="bottom_or_right" ID="ID_1263653084" CREATED="1749657527232" MODIFIED="1749657531209">
+<edge COLOR="#00ffff"/>
+<node TEXT="应急预案" ID="ID_1895522535" CREATED="1749657543039" MODIFIED="1749657554221"/>
+<node TEXT="持仓分散度管理" ID="ID_987751205" CREATED="1749657560477" MODIFIED="1749657566540"/>
+<node TEXT="保留现金" ID="ID_1308339011" CREATED="1749657568848" MODIFIED="1749657572895">
+<node TEXT="不空仓不满仓" ID="ID_1905757282" CREATED="1749657583288" MODIFIED="1749657588624"/>
+</node>
+</node>
+</node>
+</map>
