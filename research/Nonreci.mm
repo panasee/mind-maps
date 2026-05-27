2377921456cd4ee059c82aebef4afa1af8de6b08
panasee <dongkai.pan@gmail.com>
2026-05-27 17:33:48 +0800

    Update financial and research mind maps

--- research/Nonreci.mm
+++ research/Nonreci.mm
@@ -1,14 +1,10 @@
-<map version="freeplane 1.12.1">
+<map version="freeplane 1.11.5">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
-<bookmarks>
-    <bookmark nodeId="ID_1090958577" name="Root" opensAsRoot="true"/>
-</bookmarks>
 <node TEXT="Sparkles" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1651732063788">
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
 <font BOLD="true"/>
-<hook NAME="MapStyle" background="#f9f9f8ff" zoom="1.21">
-    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_tags="UNDER_NODES" show_note_icons="true" associatedTemplateLocation="template:/light_nord_template.mm" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
-    <tags category_separator="::"/>
+<hook NAME="MapStyle" background="#f9f9f8" zoom="0.826">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/light_nord_template.mm" fit_to_viewport="false"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
@@ -23,9 +19,6 @@
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
 <font SIZE="11" BOLD="false" ITALIC="false"/>
-</stylenode>
-<stylenode LOCALIZED_TEXT="defaultstyle.tags">
-<font SIZE="10"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.attributes">
 <font SIZE="10"/>
@@ -86,6 +79,131 @@
 </stylenode>
 </map_styles>
 </hook>
+<node TEXT="\latex Non-linear Optics\\&#xa;$\boldsymbol {p(E)}=\alpha^{(1)}\cdot \boldsymbol E + \alpha^{(2)}\boldsymbol{:EE} +\dots$\\&#xa;(电场很大时出现非线性, 比如激光)\\&#xa;(重点是光与物质间的相互作用)" POSITION="bottom_or_right" ID="ID_1588314871" CREATED="1651732132630" MODIFIED="1651734004005">
+<node TEXT="coherent nonlinear optics&#xa;(相干非线性光学)" ID="ID_103838554" CREATED="1651732141004" MODIFIED="1651733183572">
+<node ID="ID_67349875" CREATED="1651732157030" MODIFIED="1651735587465"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>相干调控(coherent control) </b>
+    </p>
+    <p>
+      <b>(利用控制相位来控制结果)</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="strong nonlinear optical effects&#xa;(强非线性光学效应)" ID="ID_863971684" CREATED="1651733559869" MODIFIED="1651733586269">
+<node TEXT="不能用微扰, 对应强耦合, 是物理中最难的问题" ID="ID_1791903058" CREATED="1651733590645" MODIFIED="1651734042156"/>
+</node>
+<node TEXT="photonic crystals(光子晶体)" ID="ID_652549197" CREATED="1651734213200" MODIFIED="1651734253433">
+<node TEXT="不同介电常数的材料周期排列, 存在光子带隙(禁带)" ID="ID_407802535" CREATED="1651734254845" MODIFIED="1651734301628"/>
+</node>
+<node ID="ID_1042682701" CREATED="1651734617971" MODIFIED="1651734657946"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      photonic lattices(光子晶格)
+    </p>
+    <p>
+      (周期性<b>波导</b>阵列)
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="光波在波导中的传播行为可以分解为离散波导模式的叠加" ID="ID_232827501" CREATED="1651734633782" MODIFIED="1651734703650"/>
+<node TEXT="相邻波导间存在隐失波的耦合, 导致新现象" ID="ID_800396584" CREATED="1651734703830" MODIFIED="1651734910432"/>
+<node TEXT="很容易引入非线性, 是研究各种离散现象的平台" ID="ID_398627050" CREATED="1651734910691" MODIFIED="1651734939239"/>
+</node>
+<node ID="ID_1009811571" CREATED="1651735400364" MODIFIED="1651738066198"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>被动非线性光学效应(光与介质间无能量交换):三波混频, 四波混频</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="两个光子与非线性光学材料作用后产生一个新光子(类似有媒介的三粒子散射)" ID="ID_1341913117" CREATED="1651735426017" MODIFIED="1651737753037">
+<node TEXT="\latex \textbf{利用相干性可出现二阶倍频级联(cascading)效应:\\&#xa;相位失配导致不同光子相互干涉产生能量回流&#xa;\\(类比准动量不守恒, $\Delta k = k_{2\omega}-2k_\omega-G_m\neq0$)}" ID="ID_616172174" CREATED="1651736392332" MODIFIED="1651736767640"/>
+</node>
+<node TEXT="金属的对称性禁止了该现象, 然而现实中由于表面处对称性破缺, 该现象被观测所证实" ID="ID_1835037890" CREATED="1651735601489" MODIFIED="1651735672012">
+<node TEXT="表面或者缺陷等对称破缺的特殊位置可以打破材料的对称性限制,带来新现象" ID="ID_1438180745" CREATED="1651735672384" MODIFIED="1651735718093">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+<node TEXT="光孤子(soliton)" ID="ID_633744535" CREATED="1651737008139" MODIFIED="1651737019136">
+<node ID="ID_858609189" CREATED="1651737025075" MODIFIED="1651737086644"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>光学克尔效应 </b>
+    </p>
+    <p>
+      <b>(强激光传播过程中对材料造成影响进而影响自身) </b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="主动非线性光学效应(光与介质间有能量交换):非线性折射&#xa;(折射率非常数, 可导致自聚焦/散焦)" ID="ID_598159232" CREATED="1651737322520" MODIFIED="1651737565964"/>
+<node TEXT="全光开关" ID="ID_1803187015" CREATED="1651738052526" MODIFIED="1651738058973">
+<node ID="ID_620958466" CREATED="1651738114664" MODIFIED="1651738193383"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      光子之间并没有较强的相互作用, 因此只能采用间接方式&quot;以光控光&quot;, 即<b>用一束泵浦光去改变传输信号光的介质, 从而实现对信号光的开关.</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node ID="ID_1171356710" CREATED="1651738194134" MODIFIED="1651738236323"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>声子间也不存在物理意义上的&quot;相互作用&quot;, 声子散射应当也是由材料作为中介实现的,因此可以借鉴表达方式</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="???" POSITION="bottom_or_right" ID="ID_267305220" CREATED="1651732802584" MODIFIED="1651732806712">
+<node TEXT="格波是否存在传播过程,比如如果热流通道狭小,是否会出现格波衍射? 如果材料不均, 是否会有格波折射?" ID="ID_1355686938" CREATED="1651732807514" MODIFIED="1651734995259"/>
+<node TEXT="光子可以被束缚在小空间内导致电场大幅增强, 声子局域化是否同理?" ID="ID_1488298946" CREATED="1651733333291" MODIFIED="1651733397444">
+<node TEXT="单光子非线性光学&#xa;(少数光子与单原子在微共振腔内相互作用&#xa;, 2012物理Nobel)" ID="ID_1678378905" CREATED="1651733404687" MODIFIED="1651733505117"/>
+</node>
+</node>
 <node TEXT="" POSITION="top_or_left" ID="ID_710781027" CREATED="1651732900155" MODIFIED="1730599138055">
 <node TEXT="实现物理过程的反过程都是重大创新" ID="ID_1778333462" CREATED="1651732631934" MODIFIED="1651732681301"/>
 <node TEXT="科技界有人把已知现象/过程重新命名, 又开了新的领域" ID="ID_597383838" CREATED="1651732930262" MODIFIED="1651732984917">
@@ -110,20 +228,8 @@
 </node>
 <node TEXT="二者在热力学上是共轭物理量，地位平等&#xa;（类似压强和体积，或者位置和动量）" ID="ID_1733118183" CREATED="1652531578949" MODIFIED="1652532354034"/>
 </node>
-<node TEXT="学科框架&#xa;(一个包容全部信息的底层结构，然后抽象出各种上层特征来简化这个结构的分析)" POSITION="bottom_or_right" ID="ID_1051325480" CREATED="1757243081599" MODIFIED="1757243147302">
-<node TEXT="晶体电子学" ID="ID_432121741" CREATED="1757243147943" MODIFIED="1757243151917">
-<node TEXT="底层结构：能带(本征能量)+波函数结构(本征态)" ID="ID_26048172" CREATED="1757243154102" MODIFIED="1757256700874">
-<node TEXT="能带包含了所有材料势场的信息(能量动量关系)，波函数则包含了电子自身与能带交互后的信息，比如自旋信息" ID="ID_576500656" CREATED="1757243208273" MODIFIED="1757243430793"/>
-<node TEXT="所有输运也是通过改变能带实现的，比如加电场是费米面偏移，加磁场使费米面上电子态绕等能面轨道循环，能谱量子化" ID="ID_313976990" CREATED="1757243263719" MODIFIED="1757243509446">
-<node TEXT="注意外场会破坏周期性，所以存在近似有效性问题" ID="ID_1118660527" CREATED="1757246185107" MODIFIED="1757246206865"/>
-</node>
-</node>
-<node TEXT="上层特征：对称性，特殊性质归纳" ID="ID_827947300" CREATED="1757243165438" MODIFIED="1757243300879"/>
-</node>
-<node TEXT="金融市场" ID="ID_1210899750" CREATED="1757243302979" MODIFIED="1757243361868">
-<node TEXT="底层结构：价格曲线+成交量信息" ID="ID_830795983" CREATED="1757243305626" MODIFIED="1757243320650"/>
-<node TEXT="上层特征：平均线，通道线，趋势线等等" ID="ID_1296275920" CREATED="1757243321027" MODIFIED="1757243341802"/>
-</node>
+<node TEXT="edge computing" POSITION="top_or_left" ID="ID_1024734945" CREATED="1730599146700" MODIFIED="1730599150137">
+<node TEXT="本地计算降低网络需求" ID="ID_1399557433" CREATED="1730599153756" MODIFIED="1730599227610"/>
 </node>
 </node>
 </map>
