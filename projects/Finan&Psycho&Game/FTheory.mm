f42d0df541933d3ebeab2715710fd380b0a48a17
panasee <dongkai.pan@gmail.com>
2026-09-13 23:59:35 +0800

    Separate finance assumptions and tidy the theory map

--- projects/Finan&Psycho&Game/FTheory.mm
+++ projects/Finan&Psycho&Game/FTheory.mm
@@ -1,11 +1,11 @@
-<map version="freeplane 1.12.1">
+<map version="freeplane 1.12.15">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
 <bookmarks>
     <bookmark nodeId="ID_696401721" name="Root" opensAsRoot="true"/>
 </bookmarks>
 <node TEXT="Theories" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1755317847889" STYLE="oval">
 <font SIZE="18"/>
-<hook NAME="MapStyle" zoom="1.4641001">
+<hook NAME="MapStyle" zoom="1.3310001">
     <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" auto_compact_layout="true" show_tags="UNDER_NODES" associatedTemplateLocation="template:/standard-1.6.mm" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
     <tags category_separator="::"/>
 
@@ -13,11 +13,12 @@
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
 <font SIZE="24"/>
 <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
-<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="true" COLOR="#000000" STYLE="fork" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" TEXT_WRITING_DIRECTION="LEFT_TO_RIGHT" CHILD_NODES_LAYOUT="AUTO" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#808080" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="SOLID">
 <arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
-<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<font NAME="SimSun" SIZE="12" BOLD="false" UNDERLINED="false" STRIKETHROUGH="false" ITALIC="false"/>
 <richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
 <richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<edge COLOR="#808080"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.details"/>
 <stylenode LOCALIZED_TEXT="defaultstyle.tags">
@@ -56,7 +57,7 @@
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
 <font SIZE="16"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" FORMAT_AS_HYPERLINK="false" COLOR="#00b439">
 <font SIZE="14"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
@@ -80,16 +81,16 @@
 <node TEXT="传统金融学" POSITION="bottom_or_right" ID="ID_961200986" CREATED="1755315610021" MODIFIED="1755317863757">
 <edge COLOR="#00ff00"/>
 <node TEXT="\latex 核心前提：无套利假设\\&#xa;(无套利用于理论模型)" ID="ID_1618105316" CREATED="1755315703978" MODIFIED="1755317902279">
-<node TEXT="无套利假设是经典金融理论的基础&#xa;（市场无摩擦，投资者完全理性）" POSITION="bottom_or_right" ID="ID_244038850" CREATED="1755315619733" MODIFIED="1755315770636">
-<node TEXT="假设市场是有效的，投资者是理性的，任何套利机会都会被迅速消除，因此资产价格会回归其“真实价值”" ID="ID_1631654328" CREATED="1755315647075" MODIFIED="1755315648265"/>
+<node TEXT="无套利假设是经典金融理论的基础（市场无摩擦）" POSITION="bottom_or_right" ID="ID_244038850" CREATED="1755315619733" MODIFIED="1789305061203" AI_EDITS="true"/>
+</node>
+<node TEXT="有效市场与理性投资者假设" ID="ID_1975447374" CREATED="1789305053209" MODIFIED="1789305053209" AI_EDITS="true">
+<node TEXT="假设市场是有效的，投资者是理性的，任何套利机会都会被迅速消除，因此资产价格会回归其“真实价值”" ID="ID_1631654328" CREATED="1755315647075" MODIFIED="1789305784382">
+<font NAME="SimSun"/>
 </node>
 </node>
 <node TEXT="风险中性定价理论&#xa;（基于无套利原则）" ID="ID_248561503" CREATED="1755316005158" MODIFIED="1755316095422">
 <node TEXT="风险中性世界: 投资者不关心风险，他们只看重预期回报。因此，他们对所有投资，无论风险高低，都只要求一个回报率——无风险利率 (r)" ID="ID_1482945682" CREATED="1755316019167" MODIFIED="1755316036851">
 <node TEXT="一只股票的预期回报率，不是由其自身的风险（波动性）和投资者的风险偏好决定的，而是直接等于无风险利率。&#xa;一个期权的预期回报率，同样也等于无风险利率。" ID="ID_1265492750" CREATED="1755316122048" MODIFIED="1755316126416"/>
-<node TEXT="" ID="ID_689552801" CREATED="1755316271172" MODIFIED="1755316467047">
-<hook URI="5.png" SIZE="0.7741935" NAME="ExternalObject"/>
-</node>
 </node>
 <node TEXT="核心点：将复杂的、难以预测的“真实世界”期望回报（包含风险溢价）问题，巧妙地转化为了简单的、用无风险利率来折现“风险中性世界”期望回报的问题，提供定价工具(比如BS)" ID="ID_1398689403" CREATED="1755316139871" MODIFIED="1755316168562"/>
 </node>
@@ -112,15 +113,9 @@
 </node>
 </node>
 <node TEXT="\latex 几何布朗运动: $dS_t=\mu S_tdt+\sigma S_tdW_t$\\&#xa;使用伊藤引理，$f=\ln S\Rightarrow d(\ln S)=(\mu-\frac{\sigma^2}2)dt+\sigma dW$" ID="ID_1443059666" CREATED="1755317971181" MODIFIED="1755321787570">
-<arrowlink DESTINATION="ID_648799398" STARTINCLINATION="1.2 pt;-27 pt;" ENDINCLINATION="-34.8 pt;4.2 pt;"/>
-<node TEXT="" ID="ID_1752594496" CREATED="1755321878417" MODIFIED="1755321878417">
-<hook NAME="FirstGroupNode"/>
-</node>
+<arrowlink DESTINATION="ID_648799398" STARTINCLINATION="1.5 pt;-26.25 pt;" ENDINCLINATION="-33.75 pt;4.5 pt;"/>
 <node TEXT="\latex $\mu$:期望收益率" ID="ID_977313293" CREATED="1755318038431" MODIFIED="1755321759518"/>
-<node TEXT="\latex 漂移系数（增长速率）: $\mu-0.5\sigma^2\neq \mu$\\&#xa;为连续复利期望收益率" ID="ID_1422013865" CREATED="1755321651179" MODIFIED="1755321745570"/>
-<node TEXT="" ID="ID_1189308255" CREATED="1755321878416" MODIFIED="1755321880973">
-<hook NAME="SummaryNode"/>
-<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="\latex 漂移系数（增长速率）: $\mu-0.5\sigma^2\neq \mu$\\&#xa;为连续复利期望收益率" ID="ID_1422013865" CREATED="1755321651179" MODIFIED="1755321745570">
 <node TEXT="二者是收益率序列算数平均和几何平均的区别&#xa;(5%, 13%, 9%)对应算数平均增长率为9%，而实际的几何平均增长率为8.95%，差值为波动导致" ID="ID_1877102078" CREATED="1755321878418" MODIFIED="1755322186948" HGAP_QUANTITY="28.4 pt"/>
 </node>
 <node TEXT="\latex 股票价格服从对数正态分布\\&#xa;$\ln(S_T)\sim N(\ln(S_0)+(\mu-\frac12\sigma^2)T, \sigma^2T)$" ID="ID_1584560263" CREATED="1755318089386" MODIFIED="1755318154917"/>
